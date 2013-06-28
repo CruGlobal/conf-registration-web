7bb0263566a1b8224c981a4727ec77613ce6707f
robbyronk <robby.ronk@gmail.com>
2013-06-28 17:47:32 +0000

    Remove register route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,15 +7,6 @@ angular.module('confRegistrationWebApp', ['ngMockE2E', 'ngResource'])
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/register/:conferenceId', {
-        controller: 'RegisterCtrl',
-        template: '',
-        resolve: {
-          conference: ['$route', 'Conferences', function (Conferences, $route) {
-            return Conferences.getById($route.current.params.conferenceId); // todo handle a not found conference
-          }]
-        }
-      })
       .when('/register/:conferenceId/page/:pageId', {
         templateUrl: 'views/page.html',
         controller: 'PageCtrl',
--- app/scripts/controllers/register.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('confRegistrationWebApp')
-  .controller('RegisterCtrl', function ($scope, conference, $location) {
-    $location.path('/register/' + conference.id + '/page/' + conference.pages[0].id);
-  });
