174092c248990b3cb715a89301f0ef149e8e5271
robbyronk <robby.ronk@gmail.com>
2013-06-28 10:27:02 +0000

    resolve needs to be explicitly injected

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,18 +11,18 @@ angular.module('confRegistrationWebApp', [])
         controller: 'RegisterCtrl',
         template: '',
         resolve: {
-          conference: function (Conferences, $route) {
+          conference: ['$route', 'Conferences', function (Conferences, $route) {
             return Conferences.getById($route.current.params.conferenceId); // todo handle a not found conference
-          }
+          }]
         }
       })
       .when('/register/:conferenceId/page/:pageId', {
         templateUrl: 'views/page.html',
         controller: 'PageCtrl',
         resolve: {
-          conference: function ($route, Conferences) {
+          conference: ['$route', 'Conferences', function ($route, Conferences) {
             return Conferences.getById($route.current.params.conferenceId); // todo handle a not found conference
-          }
+          }]
         }
       })
       .otherwise({
