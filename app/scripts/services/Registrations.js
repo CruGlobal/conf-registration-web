40077cd118e800b07a36a5165a51e2952280a8f8
robbyronk <robby.ronk@gmail.com>
2013-06-30 16:26:35 +0000

    Split up all registrations and current registration

--- app/scripts/services/Registrations.js
+++ app/scripts/services/Registrations.js
@@ -4,8 +4,14 @@ angular.module('confRegistrationWebApp')
   .factory('Registrations', function ($resource, $http) {
     var Registrations = $resource('registrations/:id');
 
-    Registrations.getForConference = function (conferenceId) {
+    Registrations.getCurrentForConference = function (conferenceId) {
       return $http.get('conferences/' + conferenceId + '/registrations/current').then(function (response) {
+        return response.data;
+      });
+    };
+
+    Registrations.getAllForConference = function (conferenceId) {
+      return $http.get('conferences/' + conferenceId + '/registrations').then(function (response) {
         return response.data;
       });
     };
