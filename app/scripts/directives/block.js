4fc5d2be8b17245533a250e370300174ba9f11f9
robbyronk <robby.ronk@gmail.com>
2013-06-30 16:20:19 +0000

    Load answers from directive parameters

--- app/scripts/directives/block.js
+++ app/scripts/directives/block.js
@@ -10,7 +10,7 @@ angular.module('confRegistrationWebApp')
         'prefillAnswer': '=answer'
       },
       controller: function ($scope) {
-        $scope.answer = {};
+        $scope.answer = angular.copy($scope.prefillAnswer) || {};
 
         $scope.updateAnswer = function () {
 //          $scope.answer.$save();
