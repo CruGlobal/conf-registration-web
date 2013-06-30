e738bc6d89b0a5e97ebdff4ebb0f114ead53f93c
robbyronk <robby.ronk@gmail.com>
2013-06-30 16:11:57 +0000

    Move answer and updateAnswer function to block scope

--- app/scripts/directives/textQuestion.js
+++ app/scripts/directives/textQuestion.js
@@ -4,14 +4,9 @@ angular.module('confRegistrationWebApp')
   .directive('textQuestion', function () {
     return {
       templateUrl: 'views/textQuestion.html',
-      controller: function ($scope) {
-        $scope.updateAnswer = function () {
-          console.log('block ' + $scope.block.id + ' answer changed to ' + $scope.answer);
-        };
-      },
       restrict: 'E',
       link: function (scope, elements) {
-        elements.find('input').bind('blur', function () { // todo this is fragile and will break if the template changes
+        elements.find('input').bind('blur', function () {
           scope.updateAnswer();
         });
       }
