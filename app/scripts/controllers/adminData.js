006a7176ee403329c952d42b07756b7d67305c5a
Adam Meyer <adam.meyer@cru.org>
2013-07-12 17:11:28 -0400

    Added sortable table.

--- app/scripts/controllers/adminData.js
+++ app/scripts/controllers/adminData.js
@@ -5,6 +5,7 @@ angular.module('confRegistrationWebApp')
     $scope.conference = conference;
 
     $scope.blocks = [];
+    $scope.reversesort=false;
 
     angular.forEach(conference.pages, function (page) {
       angular.forEach(page.blocks, function (block) {
@@ -18,6 +19,23 @@ angular.module('confRegistrationWebApp')
       return _.find(registration.answers, function (answer) {
         return angular.equals(answer.block, blockId);
       });
+    };
+
+    $scope.answerSort = function(registration) {
+        if(angular.isDefined($scope.order)){
+            if(angular.isDefined($scope.findAnswer(registration, $scope.order))){ return $scope.findAnswer(registration, $scope.order).value };
+        }else{
+            return 0;
+        }
+    };
+
+    $scope.setOrder = function(order) {
+        if(order==$scope.order){
+            $scope.reversesort=!$scope.reversesort;
+        }else{
+            $scope.reversesort=false;
+        }
+        $scope.order = order;
     };
 
     $scope.registrations = registrations;
