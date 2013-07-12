db988b17daf7222fbff28cd297a9747eb4282e6f
robbyronk <robby.ronk@gmail.com>
2013-07-12 11:15:57 +0000

    Refactor

--- app/scripts/services/ConfCache.js
+++ app/scripts/services/ConfCache.js
@@ -4,31 +4,33 @@ angular.module('confRegistrationWebApp')
   .service('ConfCache', function ConfCache($cacheFactory, $rootScope, $http, $q) {
     var cache = $cacheFactory('conf');
 
-    this.query = function (id) {
-      var path = 'conferences/' + (id || '');
-      var cached = cache.get(path);
-      if (angular.isDefined(cached)) {
-        $rootScope.$broadcast(path, cached);
+    var path = function (id) {
+      return 'conferences/' + (id || '');
+    };
+
+    var checkCache = function (path, callback) {
+      var cachedConferences = cache.get(path);
+      if (angular.isDefined(cachedConferences)) {
+        callback(cachedConferences, path);
       } else {
         $http.get(path).success(function (conferences) {
           cache.put(path, conferences);
-          $rootScope.$broadcast(path, conferences);
+          callback(conferences, path);
         });
       }
     };
 
+    this.query = function (id) {
+      checkCache(path(id), function (conferences, path) {
+        $rootScope.$broadcast(path, conferences);
+      })
+    };
+
     this.get = function (id) {
       var defer = $q.defer();
-      var path = 'conferences/' + (id || '');
-      var cached = cache.get(path);
-      if (angular.isDefined(cached)) {
-        defer.resolve(cached);
-      } else {
-        $http.get(path).success(function (conferences) {
-          cache.put(path, conferences);
-          defer.resolve(conferences);
-        });
-      }
+      checkCache(path(id), function (conferences) {
+        defer.resolve(conferences);
+      });
       return defer.promise;
     }
   });
