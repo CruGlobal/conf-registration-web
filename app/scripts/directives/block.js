d2d6a0928757d9a875c0cbc13d6143b0a99f9832
Ryan Carlson <ryan.t.carlson@cru.org>
2013-08-13 16:07:23 -0400

    Changing this code back to the promise model. (tested, works)

--- app/scripts/directives/block.js
+++ app/scripts/directives/block.js
@@ -6,21 +6,21 @@ angular.module('confRegistrationWebApp')
       templateUrl: 'views/blockDirective.html',
       restrict: 'E',
       controller: function ($scope, AnswerCache, RegistrationCache, uuid) {
-        var currentRegistration = RegistrationCache.getCurrentRightNow($scope.conference.id);
-        var answerForThisBlock = _.where(currentRegistration.answers, { 'blockId': $scope.block.id });
-        if (answerForThisBlock.length > 0) {
-          $scope.answer = answerForThisBlock[0];
-        }
-        if (!angular.isDefined($scope.answer)) {
-          $scope.answer = {
-            id : uuid(),
-            registrationId : currentRegistration.id,
-            blockId : $scope.block.id,
-            value : {}
-          };
-          currentRegistration.answers.push($scope.answer);
-        }
-
+        RegistrationCache.getCurrent($scope.conference.id).then(function(currentRegistration){
+          var answerForThisBlock = _.where(currentRegistration.answers, { 'blockId': $scope.block.id });
+          if (answerForThisBlock.length > 0) {
+            $scope.answer = answerForThisBlock[0];
+          }
+          if (angular.isUndefined($scope.answer)) {
+            $scope.answer = {
+              id : uuid(),
+              registrationId : currentRegistration.id,
+              blockId : $scope.block.id,
+              value : {}
+            };
+            currentRegistration.answers.push($scope.answer);
+          }
+        });
         AnswerCache.syncBlock($scope, 'answer');
       }
     };
---
+++ app/scripts/services/authorizationInterceptor.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: ryancarlson
+ * Date: 8/13/13
+ * Time: 3:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
