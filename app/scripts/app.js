6e663d9f56defcfc18fa425cb527c02d36baca44
Ryan Carlson <ryan.t.carlson@cru.org>
2013-08-13 18:35:23 -0400

    Include the new authorization interceptor  - include it on index.html as a script import  - include it in the app.js by referencing it as an http interceptor

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -84,4 +84,5 @@ angular.module('confRegistrationWebApp', ['ngResource', 'ngCookies', 'ui.bootstr
   .config(function ($httpProvider) {
     $httpProvider.interceptors.push('currentRegistrationInterceptor');
     $httpProvider.interceptors.push('httpUrlInterceptor');
+    $httpProvider.interceptors.push('authorizationInterceptor');
   });
