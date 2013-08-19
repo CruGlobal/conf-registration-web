417f4b4582c7d099a78ba7a7803bf7dbb26d3543
Adam Meyer <adam.meyer@cru.org>
2013-08-19 16:23:18 -0400

    jshint fixes

--- app/scripts/directives/formDropArea.js
+++ app/scripts/directives/formDropArea.js
@@ -10,81 +10,80 @@ angular.module('confRegistrationWebApp')
           $scope.moveType = x.moveType;
         });
 
-        $scope.moveBlock = function(blockId, newPage, newPosition) {
-          var tempPositionArray=new Array();
+        $scope.moveBlock = function (blockId, newPage, newPosition) {
+          var tempPositionArray = [];
           var newPageIndex;
-          $scope.conference.registrationPages.forEach(function(page, pageIndex){
-            if(newPage==page.id){
-              newPageIndex=pageIndex;
+          $scope.conference.registrationPages.forEach(function (page, pageIndex) {
+            if (newPage === page.id) {
+              newPageIndex = pageIndex;
             }
-            page.blocks.forEach(function(block, blockIndex){
-              tempPositionArray[block.id]=new Object({page:pageIndex,block:blockIndex});
+            page.blocks.forEach(function (block, blockIndex) {
+              tempPositionArray[block.id] = new Object({page: pageIndex, block: blockIndex});
             });
           });
           //console.log('=======MOVE BLOCK==========',blockId, newPageIndex, newPosition);
-
-          var origBlock=$scope.conference.registrationPages[tempPositionArray[blockId].page].blocks[tempPositionArray[blockId].block];
+          var origPage = tempPositionArray[blockId].page;
+          var origBlock = $scope.conference.registrationPages[origPage].blocks[tempPositionArray[blockId].block];
           $scope.deleteBlock(blockId);
-          origBlock.pageId=newPage;  //Update page id
-          $scope.conference.registrationPages[newPageIndex].blocks.splice(newPosition,0,origBlock);
+          origBlock.pageId = newPage;  //Update page id
+          $scope.conference.registrationPages[newPageIndex].blocks.splice(newPosition, 0, origBlock);
         };
 
-        $scope.insertBlock = function(blockType, newPage, newPosition) {
-          var tempPositionArray=new Array();
+        $scope.insertBlock = function (blockType, newPage, newPosition) {
+          var tempPositionArray = [];
           var newPageIndex;
-          $scope.conference.registrationPages.forEach(function(page, pageIndex){
-            if(newPage==page.id){
-              newPageIndex=pageIndex;
+          $scope.conference.registrationPages.forEach(function (page, pageIndex) {
+            if (newPage === page.id) {
+              newPageIndex = pageIndex;
             }
-            page.blocks.forEach(function(block, blockIndex){
-              tempPositionArray[block.id]=new Object({page:pageIndex,block:blockIndex});
+            page.blocks.forEach(function (block, blockIndex) {
+              tempPositionArray[block.id] = new Object({page: pageIndex, block: blockIndex});
             });
           });
-          console.log('=======NEW BLOCK==========',blockType, newPageIndex, newPosition);
+          //console.log('=======NEW BLOCK==========',blockType, newPageIndex, newPosition);
 
-          var newBlock=new Object({
+          var newBlock = new Object({
             id: uuid(),
-            content: "",
+            content: '',
             pageId: newPage,
             required: false,
-            title: "New Question",
+            title: 'New Question',
             type: blockType
           });
 
           $scope.$apply(function (scope) {
-            scope.conference.registrationPages[newPageIndex].blocks.splice(newPosition,0,newBlock);
+            scope.conference.registrationPages[newPageIndex].blocks.splice(newPosition, 0, newBlock);
           });
 
         };
 
-        $scope.deleteBlock = function(blockId) {
-          var tempPositionArray=new Array();
-          $scope.conference.registrationPages.forEach(function(page, pageIndex){
-            page.blocks.forEach(function(block, blockIndex){
-              tempPositionArray[block.id]=new Object({page:pageIndex,block:blockIndex});
+        $scope.deleteBlock = function (blockId) {
+          var tempPositionArray = [];
+          $scope.conference.registrationPages.forEach(function (page, pageIndex) {
+            page.blocks.forEach(function (block, blockIndex) {
+              tempPositionArray[block.id] = new Object({page: pageIndex, block: blockIndex});
             });
           });
-          $scope.conference.registrationPages[tempPositionArray[blockId].page].blocks.splice(tempPositionArray[blockId].block,1);
+          $scope.conference.registrationPages[tempPositionArray[blockId].page].blocks.splice(
+            tempPositionArray[blockId].block,
+            1
+          );
         };
       },
-      link: function postLink(scope, element, uuid) {
+
+      link: function postLink(scope, element) {
         element.bind('drop', function (ev) {
           ev.preventDefault();
           var pageId = $(ev.target).closest('.crs-formElements').attr('data-page-id');
           var position = $(ev.target).closest('block').prevAll().length + scope.crsPositionAdd;
           if (scope.moveType === 'new') {
             var blockType = scope.blockId;
-           // var li = document.createElement('div');
-            //li.innerHTML = '<label>' + blockType + '</label>';
-//            $('#crsDropZone').before(li);
             scope.insertBlock(blockType, pageId, position);
           } else if (scope.moveType === 'move') {
             $('#' + scope.blockId).insertBefore($('#crsDropZone'));
             scope.moveBlock(scope.blockId, pageId, position);
           }
         });
-
-
 
         element.bind('dragover', function (ev) {
           ev.preventDefault();
