cf6cddd55be4d0e8675493db6f8a5d655ca0b53e
robbyronk <robby.ronk@gmail.com>
2013-08-23 16:27:59 +0000

    jshint

--- app/scripts/services/Model.js
+++ app/scripts/services/Model.js
@@ -4,7 +4,7 @@ angular.module('confRegistrationWebApp')
   .service('Model', function Model($cacheFactory, $rootScope, $http, $q) {
     var cache = $cacheFactory('cache');
 
-    var Model = this;
+    var thisModel = this;
 
     this.create = function (path, object) {
       return $http.post(path, object).then(function (response) {
@@ -13,7 +13,7 @@ angular.module('confRegistrationWebApp')
 
         cache.put(createdObjectPath, createdObject);
 
-        Model.get(path).then(function (parentCollection) {
+        thisModel.get(path).then(function (parentCollection) {
           parentCollection.push(createdObject);
           cache.put(path, parentCollection);
           $rootScope.$broadcast(path, parentCollection);
@@ -43,7 +43,7 @@ angular.module('confRegistrationWebApp')
         var parentPath = match[0];
         var removedObjectId = match[1];
 
-        Model.get(parentPath).then(function (oldParentCollection) {
+        thisModel.get(parentPath).then(function (oldParentCollection) {
           var parentCollection = _.reject(oldParentCollection, { id: removedObjectId });
           cache.put(path, parentCollection);
           $rootScope.$broadcast(path, parentCollection);
@@ -53,8 +53,8 @@ angular.module('confRegistrationWebApp')
 
     this.subscribe = function (scope, name, path) {
       scope.$watch(name, function (object) {
-        if(angular.isDefined(object)) {
-          Model.update(path, object);
+        if (angular.isDefined(object)) {
+          thisModel.update(path, object);
         }
       }, true);
 
@@ -62,13 +62,13 @@ angular.module('confRegistrationWebApp')
         scope[name] = object;
       });
 
-      Model.get(path).then(function (object) {
+      thisModel.get(path).then(function (object) {
         scope[name] = object;
       });
     };
 
     this.update = function (path, object) {
-      if(angular.equals(object, cache.get(path))) {
+      if (angular.equals(object, cache.get(path))) {
         // do nothing
       } else {
         $http.put(path, object).then(function () {
