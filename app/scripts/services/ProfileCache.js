00f64f3a8ca3714283d2dbf955186ea1a33d2341
Ryan Carlson <ryan.t.carlson@cru.org>
2013-08-23 17:19:42 -0400

    Adding ProfileCache js file.

---
+++ app/scripts/services/ProfileCache.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('confRegistrationWebApp')
+  .service('ProfileCache', function ProfileCache ($cacheFactory, $rootScope, $http, $q) {
+    var cache = $cacheFactory('profile');
+    var path = '/profile';
+
+    var checkCache = function (path, callback) {
+      var cachedObject = cache.get(path);
+      if (angular.isDefined(cachedObject)) {
+        callback(cachedObject, path);
+      } else {
+        $http.get(path).success(function (profileData) {
+          cache.put(path, profileData);
+          callback(profileData, path);
+        });
+      }
+    };
+
+    this.get = function () {
+      var defer = $q.defer();
+      checkCache(path, function (profileData) {
+        defer.resolve(profileData);
+      });
+      return defer.promise;
+    };
+  });
