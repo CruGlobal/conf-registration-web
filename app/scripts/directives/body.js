71b6043f0dede3d3c7692090ed819e6e0db464de
jamesdempsey <thejamesdempsey@gmail.com>
2013-09-18 12:59:28 -0400

    cleaning up the body directive

--- app/scripts/directives/body.js
+++ app/scripts/directives/body.js
@@ -3,13 +3,13 @@
 angular.module('confRegistrationWebApp')
   .directive('body', function () {
     var matchRouteToClass = {};
-    matchRouteToClass["/$"] = "page--home";
-    matchRouteToClass["/wizard/*."] = "page--wizard";
-    matchRouteToClass["/adminData/*."] = "page--admin";
-    matchRouteToClass["/register/*.*/page/*.*"] = "page--register";
+    matchRouteToClass['/$'] = 'page--home';
+    matchRouteToClass['/wizard/*.'] = 'page--wizard';
+    matchRouteToClass['/adminData/*.'] = 'page--admin';
+    matchRouteToClass['/register/*.*/page/*.*'] = 'page--register';
     return {
       restrict: 'E',
-      link: function postLink(scope, element, attrs) {
+      link: function postLink(scope, element) {
         scope.$on('$locationChangeSuccess', function (event, location) {
           angular.forEach(matchRouteToClass, function (cssClass, regExpString) {
             element.removeClass(cssClass);
