bfdf56645b94a208adee417e6adeac83c56f2b29
Adam Meyer <adam.meyer@cru.org>
2013-09-20 16:29:56 -0400

    made datepicker directive

--- app/scripts/controllers/adminDetails.js
+++ app/scripts/controllers/adminDetails.js
@@ -3,42 +3,4 @@
 angular.module('confRegistrationWebApp')
   .controller('AdminDetailsCtrl', function ($scope, $timeout, Model, registrations, conference) {
     Model.subscribe($scope, 'conference', 'conferences/' + conference.id);
-    $scope.conference = conference;
-
-    $scope.showWeeks = false;
-    $scope.toggleWeeks = function () {
-      $scope.showWeeks = !$scope.showWeeks;
-    };
-    $scope.minDate = ($scope.minDate) ? null : new Date();
-
-
-    $scope.openA = function () {
-      $timeout(function () {
-        $scope.openedA = true;
-      });
-    };
-    $scope.openB = function () {
-      $timeout(function () {
-        $scope.openedB = true;
-      });
-    };
-    $scope.openC = function () {
-      $timeout(function () {
-        $scope.openedC = true;
-      });
-    };
-    $scope.openD = function () {
-      $timeout(function () {
-        $scope.openedD = true;
-      });
-    };
-    $scope.openE = function () {
-      $timeout(function () {
-        $scope.openedE = true;
-      });
-    };
-
-    $scope.dateOptions = {
-      'starting-day': 0
-    };
   });
