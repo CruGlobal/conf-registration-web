c110a61e5386195a339dd18c674284eff3354fa7
ryancarlson <ryan.t.carlson@cru.org>
2013-09-26 14:08:05 -0400

    Remove console.log

--- app/scripts/directives/cruNgBlur.js
+++ app/scripts/directives/cruNgBlur.js
@@ -4,7 +4,6 @@ angular.module('confRegistrationWebApp')
   .directive('cruNgBlur', function () {
     return function (scope, elem, attrs) {
       elem.bind('blur', function () {
-        console.log(attrs.cruNgBlur);
         scope.$apply(attrs.cruNgBlur);
       });
     };
