fd681db58a17736a5b79c22857665f9df0fbe659
Adam Meyer <adam.meyer@cru.org>
2013-09-30 13:20:50 -0400

    Email Login Fix

--- app/scripts/controllers/loginDialog.js
+++ app/scripts/controllers/loginDialog.js
@@ -3,7 +3,7 @@
 angular.module('confRegistrationWebApp')
   .controller('LoginDialogCtrl', function ($scope, $window, apiUrl) {
     $scope.apiUrl = apiUrl;
-    $scope.submit = function () {
-      $window.location.href = apiUrl + 'auth/none/login?email=' + $scope.email;
+    $scope.submit = function (email) {
+      $window.location.href = apiUrl + 'auth/none/login?email=' + email;
     };
   });
