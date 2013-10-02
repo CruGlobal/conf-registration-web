72fa0d586f9b74f65cc178565bf51a11be66d94c
Adam Meyer <adam.meyer@cru.org>
2013-10-02 14:22:09 -0400

    adminDetails update, include contact and min deposit

--- app/scripts/controllers/adminDetails.js
+++ app/scripts/controllers/adminDetails.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('confRegistrationWebApp')
-  .controller('AdminDetailsCtrl', function ($scope, $timeout, Model, registrations, conference) {
+  .controller('AdminDetailsCtrl', function ($scope, $timeout, Model, conference) {
     Model.subscribe($scope, 'conference', 'conferences/' + conference.id);
+    console.log(conference);
   });
