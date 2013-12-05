4c81861fc7897a5c22812162b34bf77527c6e7a0
Adam Meyer <adam.meyer@cru.org>
2013-12-05 10:58:04 -0500

    delete conf modal

--- app/scripts/directives/adminNav.js
+++ app/scripts/directives/adminNav.js
@@ -4,6 +4,25 @@ angular.module('confRegistrationWebApp')
   .directive('adminNav', function () {
     return {
       templateUrl: 'views/adminNav.html',
-      restrict: 'A'
+      restrict: 'A',
+      controller: function ($scope, $modal) {
+        $scope.deleteConference = function (conferenceToDelete) {
+        console.log(conferenceToDelete);
+
+        $modal.open({
+          templateUrl: 'views/confirmDeleteConf.html',
+          controller: 'confirmCtrl',
+          resolve: {
+            defaultValue: function () {
+              return ' (clone)';
+            }
+          }
+        }).result.then(function (result) {
+          if(result){
+            alert('delete');
+          }
+        });
+      };
+      }
     };
   });
