9a1e9d39e99e3f56a3a74453b50da32ac374d087
ryancarlson <ryan.t.carlson@cru.org>
2014-02-20 14:06:55 -0500

    Add support for credit card refunds

--- app/scripts/controllers/paymentModal.js
+++ app/scripts/controllers/paymentModal.js
@@ -22,4 +22,34 @@ angular.module('confRegistrationWebApp')
         alert('payment failed...');
       });
     }
+
+    $scope.canBeRefunded = function(payment) {
+      var sum = 0;
+      _.each($scope.registration.pastPayments, function (prevRefund){
+        if(prevRefund.paymentType === 'CREDIT_CARD_REFUND' && prevRefund.refundedPaymentId === payment.id) {
+          sum += prevRefund.amount;
+        }
+      });
+      return payment.paymentType === 'CREDIT_CARD' && sum < payment.amount;
+    }
+
+    $scope.refund = function (payment) {
+      var refund = {
+        amount: payment.amount,
+        refundedPaymentId: payment.id,
+        registrationId: payment.registrationId,
+        paymentType: 'CREDIT_CARD_REFUND',
+        creditCardLastFourDigits: payment.creditCardLastFourDigits,
+        readyToProcess: true
+      };
+
+      $http.post('payments/', refund).success(function (){
+        $http.get('registrations/' + $scope.registration.id).success(function (data){
+          RegistrationCache.update('registrations/' + data.id, data, function (){});
+          $scope.registration = data;
+        });
+      }).error(function (reason){
+        alert('refund failed...');
+      });
+    }
   });
