35d2f4fc1344cffb358c830d451cdbf807fa5a9a
ryancarlson <ryan.t.carlson@cru.org>
2014-02-20 10:45:39 -0500

    Minor fixes:  - Fixed page from going back to dashboard  - Modal now returns updated registration to AdminDataCtrl, where the payments is array is updated  - RegistrationCache no longer throws an error

--- app/scripts/controllers/paymentModal.js
+++ app/scripts/controllers/paymentModal.js
@@ -2,7 +2,7 @@ angular.module('confRegistrationWebApp')
   .controller('paymentModal', function ($scope, $modalInstance, $http, data, RegistrationCache) {
     $scope.registration = data;
     $scope.close = function () {
-      $modalInstance.close('');
+      $modalInstance.close($scope.registration);
     };
 
     $scope.newPayment = {
@@ -13,8 +13,8 @@ angular.module('confRegistrationWebApp')
       $scope.newPayment.readyToProcess = true;
       $http.post('payments/', $scope.newPayment).success(function (){
         $http.get('registrations/' + $scope.registration.id).success(function (data){
-            RegistrationCache.update('registrations/' + data.id, data);
-            $scope.registration = data;
+          RegistrationCache.update('registrations/' + data.id, data, function (){});
+          $scope.registration = data;
         });
         $scope.addNewPayment = false;
         delete $scope.newPayment;
