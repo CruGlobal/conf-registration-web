9cd9a36cbd436d7da943b82eea2eda0f20bda398
Lee Braddock <lee.braddock@cru.org>
2014-02-26 17:36:51 -0500

    Provide initial admin create registration functionality.
    Change Registration-Type header value.
    Remove redundant line of code.
    
    Build out simple registration form.
    
    Remove log
    
    Reformat simple registration form script
    
    Build Registration to POST, including answers from form.
    
    Reload the admin registration page so as to reflect the new registration.
    
    Empty cache and reload current route in order to recognize newly created registration.
    Do this upon POST success response.
    
    Remove reference to unused injected dependency.
    
    Server will set total cost.

---
+++ app/scripts/controllers/registrationModal.js
@@ -0,0 +1,55 @@
+'use strict';
+
+angular.module('confRegistrationWebApp')
+  .controller('registrationModal', function ($scope, $modalInstance, $http, $route, conference, uuid, RegistrationCache) {
+
+    $scope.form = {};
+
+    $scope.register = function () {
+      // build registration
+      var registration = {};
+      registration.id = uuid();
+      registration.conferenceId = conference.id;
+      registration.completed = true;
+      registration.answers = [];
+      registration.pastPayments = [];
+
+      // populate registration answers from form
+      angular.forEach(conference.registrationPages, function (registrationPage) {
+        angular.forEach(registrationPage.blocks, function (block) {
+          if (block.profileType === 'EMAIL' || block.profileType === 'NAME') {
+            var answer = {
+              id: uuid(),
+              registrationId: registration.id,
+              blockId: block.id,
+              value: {}
+            };
+
+            if (block.profileType === 'EMAIL') {
+              answer.value = { text: $scope.form.email };
+            } else if (block.profileType === 'NAME') {
+              answer.value = { firstName: $scope.form.first, lastName: $scope.form.last };
+            }
+
+            registration.answers.push(answer);
+          }
+        });
+      });
+
+      $http.post('conferences/' + conference.id + '/registrations', registration, {headers: {'Registration-Type': 'on-behalf-of'}})
+        .success(function () {
+          // empty cache and reload in order to recognize the newly created registration
+          RegistrationCache.emptyCache();
+          $route.reload();
+        })
+        .error(function (data) {
+          console.log('Error: ' + data);
+        });
+
+      $modalInstance.close();
+    };
+
+    $scope.close = function () {
+      $modalInstance.close();
+    };
+  });
