6a9d1d8386a845b3025c826680b00c4576d3f092
Lee Braddock <lee.braddock@cru.org>
2014-03-03 17:21:33 -0500

    Initial commit :: export conference registrations.
    Compose output by answer block type.
    
    Order by page/block position.
    
    Add support for all block types.
    
    Refactor logic into function and renaming.
    
    Build 2D registrations array.
    Build header row column names by block title.
    
    Add RegistrationService to produce table (2 dimensional array) of registrations.
    
    Convert from two dimensional array to csv string.
    Provide csv in download.
    
    Minor mods.
    
    Refactoring
    
    Separate building of header and rows functionality (in order to sort on rows).
    
    Refactor RegistrationsService name
    
    Return array data type when retrieving answer value content.
    Fix header production by checking for block type.
    Append arrays in better fashion
    Rename admin link
    
    Install string.js
    
    Sort rows by last name.
    
    Move registrations table build into RegistrationsService.
    
    Reorganize service functions
    
    Minor refactoring.
    
    Refactoring registrations view service name.
    
    Add initial baseline export payments service
    
    Derive registrant name from answers.
    
    Format date/time.
    
    Show export payments link only if conference has cost.
    
    Format payment date/time.
    Assume conference has cost.
    
    Produce multiple rows, one per payment.
    Include name on each row.
    Include row when no payments.
    
    Factor out into ConferenceHelper and U.js (utilities) services those commonly used functions.
    
    Add sortArrayByIndex function to Utility service
    
    Provide column name on array sort.
    Change column header name.
    Add $ prefix when displaying conference cost.
    
    jshint
    
    Restore original apiUrl
    
    Account for possible null/undefined answer when searching by block id or type.
    
    Download file with <a> anchor element (only supported by Chrome).
    
    Add submit form function to utilities.
    
    Submit csv to endpoint which produces response with header that all browsers will recognize as a file to download.
    
    Refactor getContentByBlockType(...) function to overcome jshint cyclomatic complexity violation.

---
+++ app/scripts/services/RegistrationsViewService.js
@@ -0,0 +1,86 @@
+'use strict';
+
+angular.module('confRegistrationWebApp')
+  .service('RegistrationsViewService', function RegistrationsViewService(ConferenceHelper, U) {
+
+    this.getTable = function (conference, registrations) {
+
+      var table = [];
+
+      var header = getHeader(conference);
+
+      table.push(header);
+
+      var rows = getRows(conference, registrations);
+
+      // sort rows by last name
+      U.sortArrayByIndex(rows, _.findIndex(header, function (string) {
+        return angular.equals(string, 'Last');
+      }));
+
+      table.push.apply(table, rows);
+
+      return table;
+    };
+
+    var getHeader = function (conference) {
+      var blocks = ConferenceHelper.getPageBlocks(conference.registrationPages);
+
+      // header row of block titles
+      var header = getBlockTitles(blocks);
+
+      if (ConferenceHelper.hasCost(conference)) {
+        header.push('Conference Cost');
+      }
+
+      return header;
+    };
+
+    var getRows = function (conference, registrations) {
+      var rows = [];
+
+      var blocks = ConferenceHelper.getPageBlocks(conference.registrationPages);
+
+      // rows of answers
+      ConferenceHelper.getCompletedRegistrations(registrations).forEach(function (registration) {
+        var row = [];
+
+        angular.forEach(blocks, function (block) {
+          var answer = ConferenceHelper.findAnswerByBlockId(registration.answers, block.id);
+          var content = ConferenceHelper.getContentByBlockType(U.isEmpty(answer) ? answer : answer.value, block.type);
+          row.push.apply(row, content);
+        });
+
+        if (ConferenceHelper.hasCost(conference)) {
+          row.push('$' + U.getValue(registration.totalDue, '0'));
+        }
+
+        rows.push(row);
+      });
+
+      return rows;
+    };
+
+    var getBlockTitles = function (blocks) {
+      var titles = [];
+
+      angular.forEach(blocks, function (block) {
+        if (block.type === 'nameQuestion') {
+          titles.push('First');
+          titles.push('Last');
+        }
+        else if (block.type === 'addressQuestion') {
+          titles.push('Address1');
+          titles.push('Address2');
+          titles.push('City');
+          titles.push('State');
+          titles.push('Zip');
+        }
+        else {
+          titles.push(block.title);
+        }
+      });
+
+      return titles;
+    };
+  });
