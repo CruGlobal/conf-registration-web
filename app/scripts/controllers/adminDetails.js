989bf18f4e123bd28609040484408b6f4a121a15
Adam Meyer <adam.meyer@cru.org>
2014-03-10 11:58:45 -0400

    adminWizard & adminDetials, update ConfCache when Model update

--- app/scripts/controllers/adminDetails.js
+++ app/scripts/controllers/adminDetails.js
@@ -1,6 +1,13 @@
 'use strict';
 
 angular.module('confRegistrationWebApp')
-  .controller('AdminDetailsCtrl', function ($scope, $timeout, Model, conference) {
+  .controller('AdminDetailsCtrl', function ($scope, $timeout, Model, conference, ConfCache) {
     Model.subscribe($scope, 'conference', 'conferences/' + conference.id);
+
+    //Update cache
+    $scope.$watch('conference', function(conf) {
+      if(angular.isDefined(conference)){
+        ConfCache.update(conference.id, conf);
+      }
+    }, true);
   });
