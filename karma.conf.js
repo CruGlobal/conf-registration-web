a1687a38b81360eab51f0d382c13099e98351747
Adam Meyer <adam.meyer@cru.org>
2014-03-21 16:21:23 -0400

    updated karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,62 +1,34 @@
-// Karma configuration
+module.exports = function(config){
+  config.set({
+    basePath : '',
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    files : [
+      'app/bower_components/lodash/dist/lodash.js',
+      'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular-resource/angular-resource.js',
+      'app/bower_components/angular-cookies/angular-cookies.js',
+      'app/bower_components/angular-bootstrap/ui-bootstrap.js',
+      'app/scripts/*.js',
+      'app/scripts/**/*.js'
+      // 'test/mock/**/*.js',
+      //'test/spec/**/*.js'
+    ],
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/bower_components/lodash/dist/lodash.js',
-  'app/bower_components/angular/angular.js',
-  'app/bower_components/angular-mocks/angular-mocks.js',
-  'app/bower_components/angular-resource/angular-resource.js',
-  'app/bower_components/angular-cookies/angular-cookies.js',
-  'app/bower_components/angular-bootstrap/ui-bootstrap.js',
-  'app/scripts/*.js',
-  'app/scripts/**/*.js'
-  // 'test/mock/**/*.js',
-  //'test/spec/**/*.js'
-];
+    exclude : [
 
-// list of files to exclude
-exclude = [
-  'app/scripts/mockHttpBackend.js'
-];
+    ],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+    autoWatch : true,
 
-// web server port
-port = 8080;
+    frameworks: ['jasmine'],
 
-// cli runner port
-runnerPort = 9100;
+    browsers : ['Firefox'],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Firefox'];
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    plugins : [
+      //'karma-junit-reporter',
+      //'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine'
+    ]
+  })}
