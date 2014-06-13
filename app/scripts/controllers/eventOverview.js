5120070fd1945af9d4e5e5555487c7f526b4344d
Adam Meyer <adam.meyer@cru.org>
2014-06-13 16:54:51 -0400

    jshint

--- app/scripts/controllers/eventOverview.js
+++ app/scripts/controllers/eventOverview.js
@@ -12,14 +12,13 @@ angular.module('confRegistrationWebApp')
     };
 
     $scope.conference = conference;
+    var port = '';
     if ($location.$$port !== 80 && $location.$$port !== 443) {
-      var port = ':' + $location.$$port;
-    } else{
-      var port = '';
+      port = ':' + $location.$$port;
     }
     $scope.registrationUrl = $location.$$protocol + '://' + $location.$$host + port + '/#/register/' + conference.id;
 
-    $scope.refreshEventData = function() {
+    $scope.refreshEventData = function () {
       $scope.conference.completedRegistrationCount = '-';
       ConfCache.empty();
       $route.reload();
