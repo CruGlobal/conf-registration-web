6c387d168361f9eb5aa2b4377d904c1aa3a00c9f
ryancarlson <ryan.t.carlson@cru.org>
2014-08-12 23:57:29 +0200

    Much simpler solution for updating timestamps  - no more tzoffset header  - introduce moment-timezone library  - each time passed to the server is first converted to Eastern time (EST/EDT accounted for)  - each time returned from the server has a timezone included

--- app/scripts/directives/datepicker.js
+++ app/scripts/directives/datepicker.js
@@ -8,18 +8,17 @@ angular.module('confRegistrationWebApp')
       scope: {
         'localModel': '=model'
       },
-      controller: function ($timeout, $scope) {
+      controller: function ($timeout, $scope, TimestampUtil) {
         $scope.updateTimeStamp = function (timestamp) {
-          var saveDate = new Date(timestamp);
           $scope.$apply(function () {
-            $scope.localModel = saveDate;
+            var zone = moment.tz.zone('America/New_York');
+            $scope.localModel = TimestampUtil.convertToZone(timestamp,zone);
           });
         };
       },
       link: function (scope, element) {
-        var inputDate = new Date(scope.localModel);
         jQuery(element).find('.datepicker').datetimepicker({
-          defaultDate: moment(inputDate).format('MM/DD/YYYY hh:mm A')
+          defaultDate: moment.parseZone(scope.localModel).format('MM/DD/YYYY hh:mm A')
         }).on('dp.change', function (ev) {
           scope.updateTimeStamp(ev.date);
         });
