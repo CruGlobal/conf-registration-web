70587ff2cd5f3597129d6741b6c1fcbe24bacbc1
adammeyer <adam.meyer@cru.org>
2014-10-20 14:26:00 -0400

    correct money format

---
+++ app/scripts/filters/moneyFormat.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('confRegistrationWebApp')
+  .filter('moneyFormat', function () {
+    return function (num) {
+      if(angular.isUndefined(num)){
+        return '';
+      }
+      var p = num.toFixed(2).split(".");
+      return "$" + p[0].split("").reverse().reduce(function(acc, num, i, orig) {
+          return  num + (i && !(i % 3) ? "," : "") + acc;
+      }, "") + "." + p[1];
+    };
+  });
