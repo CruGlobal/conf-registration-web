d8fa27e79c4eb664c128f725a1f8266d930d94f4
ryancarlson <ryan.t.carlson@cru.org>
2014-11-13 14:03:03 -0500

    update fixme

--- app/scripts/controllers/eventDashboard.js
+++ app/scripts/controllers/eventDashboard.js
@@ -84,7 +84,7 @@ angular.module('confRegistrationWebApp')
             $http.get('conferences/' + conferenceToClone.id).success(function (result) {
               //clone conference details
               conference = angular.copy(result);
-              //fixme: i don't think this is right
+              //fixme: i don't think this does anything. contactUser is not an object on the conference object
               conference.contactUser = conferenceOrig.contactUser;
               conference.id = conferenceOrig.id;
               conference.name = conferenceName;
