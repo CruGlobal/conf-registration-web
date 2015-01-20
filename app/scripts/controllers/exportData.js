29abb41b23094053498dacf194aa6ecfb91b83ee
adammeyer <adam.meyer@cru.org>
2015-01-20 15:23:30 -0500

    authToken misspell

--- app/scripts/controllers/exportData.js
+++ app/scripts/controllers/exportData.js
@@ -4,7 +4,7 @@ angular.module('confRegistrationWebApp')
   .controller('exportDataModal', function ($scope, $modalInstance, $cookies, conference, apiUrl, hasCost) {
     $scope.conference = conference;
     $scope.apiUrl = apiUrl;
-    $scope.authTocken = $cookies.crsToken;
+    $scope.authToken = $cookies.crsToken;
     $scope.hasCost = hasCost;
 
     $scope.close = function () {
