f7e7af2c3dbfaacdb1795679cefa06cb88f01c72
adammeyer <adam.meyer@cru.org>
2015-01-20 17:03:06 -0500

    Date picker for registration form

--- app/scripts/directives/datepicker.js
+++ app/scripts/directives/datepicker.js
@@ -6,19 +6,29 @@ angular.module('confRegistrationWebApp')
       templateUrl: 'views/components/datepicker.html',
       restrict: 'E',
       scope: {
-        'localModel': '=model'
+        'localModel': '=model',
+        'dateOnly': '=dateOnly'
       },
       controller: function ($timeout, $scope) {
         $scope.updateTimeStamp = function (timestamp) {
           $scope.$apply(function () {
-            $scope.localModel = moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
+            if($scope.dateOnly){
+              $scope.localModel = moment(timestamp).format('YYYY-MM-DD');
+            }else{
+              $scope.localModel = moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
+            }
           });
         };
       },
       link: function (scope, element) {
         var datePickerElement = jQuery(element).find('.datepicker');
+        var dateFormat = 'MM/DD/YYYY hh:mm A';
+        if(scope.dateOnly){
+          dateFormat = 'M/D/YYYY';
+        }
         datePickerElement.datetimepicker({
-          defaultDate: moment(scope.localModel).format('MM/DD/YYYY hh:mm A')
+          defaultDate: moment(scope.localModel),
+          format: dateFormat
         }).on('dp.change', function (ev) {
           scope.updateTimeStamp(ev.date);
         });
