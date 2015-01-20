fecf63dcd1f885097fae0f32af172cdf25838dff
Scotty Waggoner <ozzieorca@gmail.com>
2015-01-19 17:08:08 -0800

    Show errors on each input when invalid regardless of other inputs in the group. Show group errors on label when any input in the group is invalid.

--- app/scripts/directives/showErrors.js
+++ app/scripts/directives/showErrors.js
@@ -15,20 +15,19 @@ angular.module('confRegistrationWebApp')
         }
 
         scope.$watch(function(){
-            if(!scope.inputs){
-              return ngModelCtrl.$invalid && ngModelCtrl.$touched;
-            }else{
-              //if we are handling a group of inputs and any of the inputs are invalid and touched
-              return scope.inputs.some(function(currentValue){
-                  return currentValue.$invalid;
-                }) &&
-                scope.inputs.every(function(currentValue){
-                  return currentValue.$touched;
-                });
-            }
+            return ngModelCtrl.$invalid && ngModelCtrl.$touched;
           },
           function(invalid){
-            element.parents('.form-group').toggleClass('has-error', invalid);
+            if(scope.inputs && scope.inputs.length >= 2){
+              //if we are handling a group of inputs and any of the inputs are invalid and touched
+              var groupInvalid = scope.inputs.some(function(currentValue){
+                  return currentValue.$invalid && currentValue.$touched;
+                });
+              element.toggleClass('has-no-error', !invalid);
+              element.parents('.form-group').toggleClass('has-error', groupInvalid);
+            }else{
+              element.parents('.form-group').toggleClass('has-error', invalid);
+            }
           }
         );
       }
