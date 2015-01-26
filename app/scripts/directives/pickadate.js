9bc0c83ca3010c119005a03fe3b6b208d22009f3
Scotty Waggoner <ozzieorca@gmail.com>
2015-01-26 01:49:26 -0800

    Allowed the date format displayed on the picker input element and the date format commited to the model to be different. Implemented required, disabled, and clearing values correctly

--- app/scripts/directives/pickadate.js
+++ app/scripts/directives/pickadate.js
@@ -1,20 +1,53 @@
 'use strict';
 
 angular.module('confRegistrationWebApp')
-  .directive('pickADate', function () {
+  .directive('pickADate', function() {
     return {
       templateUrl: 'views/components/pickadate.html',
       restrict: 'E',
+      require: 'ngModel',
       scope: {
-        'model': '=model',
-        'required': '=required',
-        'disabled': '=disabled'
+        'disabled': '='
       },
-      link: function (scope, element) {
-        $(element).find('input').pickadate({
-          format: 'yyyy/mm/dd',
+      link: function (scope, element, attr, ngModelController) {
+        //load date value from model into datepicker
+        var loaded = false;
+        scope.$watch(function(){
+            return ngModelController.$viewValue;
+          },
+          function(value){
+            if(!loaded && !_.isEmpty(value)){ //only set value of picker to value of model once when directive is first loaded
+              scope.picker.set('select', value, { format: 'yyyy-mm-dd', muted: true });
+              loaded = true;
+            }
+          });
+
+        //initialize datepicker
+        scope.picker = $(element).find('input').first().pickadate({
+          format: 'mmm d, yyyy',
           selectYears: true
-        });
+        }).pickadate('picker');
+
+        //when date is chosen, update model
+        var onSet = function(){
+          scope.$apply(function(){
+            ngModelController.$setViewValue(scope.picker.get('select', 'yyyy-mm-dd'));
+          });
+        };
+        scope.picker.on('set', onSet);
+
+        //disable picker when disabled attribute is true
+        scope.$watch(function(){
+            return scope.disabled;
+          },
+          function(disabled){
+            if (disabled){
+              scope.picker.stop();  //this destroys the onSet event callback
+            }else{
+              scope.picker.start();
+              scope.picker.on('set', onSet);
+            }
+          });
       }
     };
   });
