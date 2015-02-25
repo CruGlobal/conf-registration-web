f78413219bb58e24e290634d7589a6d6f41035a4
adammeyer <adam.meyer@cru.org>
2015-02-25 16:50:35 -0500

    correct autoFocus directive

--- app/scripts/directives/autoFocus.js
+++ app/scripts/directives/autoFocus.js
@@ -2,13 +2,11 @@
 
 angular.module('confRegistrationWebApp').directive('autoFocus', function($timeout) {
   return {
-    link: {
-      restrict: 'A',
-      post: function postLink(scope, element) {
+    restrict: 'A',
+    link: function postLink(scope, element) {
         $timeout(function () {
           element[0].focus();
         }, 300);
       }
-    }
   };
 });
