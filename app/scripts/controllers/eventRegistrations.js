3172d1ed23a4975ece9d8351069cd30f53983d01
adammeyer <adam.meyer@cru.org>
2015-03-16 10:05:43 -0400

    check for undefined registrations

--- app/scripts/controllers/eventRegistrations.js
+++ app/scripts/controllers/eventRegistrations.js
@@ -206,6 +206,9 @@ angular.module('confRegistrationWebApp')
     $scope.paymentStatus = function (registrant) {
       var registration = _.find(registrations, { 'id': registrant.registrationId });
       var paymentCategory = _.find($scope.paymentCategories, { 'name': $scope.currentPaymentCategory });
+      if(angular.isUndefined(registration)){
+        return false;
+      }
       return paymentCategory.matches(registration.totalPaid, registration.calculatedTotalDue);
     };
 
