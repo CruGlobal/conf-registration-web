542eb59de968321190af09d78830edd57a174cd0
ryancarlson <ryan.t.carlson@cru.org>
2015-03-27 10:33:44 -0400

    change sdk suffix  - really just doing this change to see if Travis will pick it up in the private repo

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -253,7 +253,7 @@ angular.module('confRegistrationWebApp', ['ngRoute', 'ngCookies', 'ngFacebook', 
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) {return;}
       js = d.createElement(s); js.id = id;
-      js.src = '//connect.facebook.net/en_US/sdk.js';
+      js.src = '//connect.facebook.net/en_US/all.js';
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
   })
