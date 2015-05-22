f68623757bf9b19b29273ff20a9fad2bfbcd9e7e
Scotty Waggoner <ozzieorca@gmail.com>
2015-05-22 11:18:24 -0700

    Remove code in accessEvent that processes data as an object. This will be added later when API changes are complete

--- app/scripts/controllers/accessEvent.js
+++ app/scripts/controllers/accessEvent.js
@@ -21,13 +21,7 @@ angular.module('confRegistrationWebApp')
       }).success(function () {
         $modalInstance.close();
       }).error(function (data) {
-        var errorMessage;
-        if(data.msg !== undefined){
-          errorMessage = data.msg;
-        }else{
-          errorMessage = data;
-        }
-        modalMessage.error('Error: ' + errorMessage);
+        modalMessage.error('Error: ' + data);
       });
     };
 
