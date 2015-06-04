8757036283a09dd59517a98a646b9fb22b18182b
adammeyer <adam.meyer@cru.org>
2015-06-04 16:23:03 -0400

    Remove pageForm.$valid watch

--- app/scripts/directives/page.js
+++ app/scripts/directives/page.js
@@ -7,9 +7,6 @@ angular.module('confRegistrationWebApp')
       restrict: 'E',
       controller: function ($scope, $location) {
         $scope.wizard = $location.path().indexOf('eventForm') !== -1;
-        $scope.$watch('pageForm.$valid', function (valid) {
-          $scope.$emit('pageValid', valid);
-        });
       }
     };
   });
