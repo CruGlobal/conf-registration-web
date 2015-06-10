46467d2ed407c7badc5a1bb2536f1024b6fac9b9
adammeyer <adam.meyer@cru.org>
2015-06-10 14:34:04 -0400

    New landing

--- app/scripts/controllers/landing.js
+++ app/scripts/controllers/landing.js
@@ -11,30 +11,61 @@ angular.module('confRegistrationWebApp')
       footer: true
     };
 
-    $scope.userIsLoggedIn = function(){
-      return angular.isDefined($cookies.crsToken) && $cookies.crsAuthProviderType !== 'NONE';
-    };
-
-    $scope.login = function(provider){
-      $cookies.intendedRoute = '/eventDashboard';
-      if(provider === 'RELAY'){
-        $window.location.href = apiUrl + 'auth/relay/login?logoutCallbackUrl=' + apiUrl + 'auth/relay/logout';
-      }else if(provider === 'FACEBOOK'){
-        $window.location = apiUrl + 'auth/facebook/authorization';
-      }
-    };
+    var userIsLoggedIn = angular.isDefined($cookies.crsToken) && $cookies.crsAuthProviderType !== 'NONE';
+    var loggedInUserEvents = [];
+    if(userIsLoggedIn){
+      $http.get('conferences').then(function(response){
+        loggedInUserEvents = _.pluck(response.data, 'id');
+      });
+    }
 
     $scope.eventSearch = function(val) {
-      return $http.get('conferences', {
+      $http.get('conferences', {
         params: {
           conferenceName: val
         }
       }).then(function(response){
-        return response.data;
+        $scope.eventFilters = {
+          locationName: '',
+          date: ''
+        };
+        $scope.searchComplete = val;
+        $scope.searchResults = response.data;
       });
     };
 
-    $scope.selectEvent = function(item){
-      $location.path('/register/' + item.id + '/page/');
+    $scope.dateFilter = function(event){
+      var eventStartTime = moment.tz(event.eventStartTime, event.eventTimezone);
+
+      switch($scope.eventFilters.date){
+        case '':
+          return true;
+        case 'This Week':
+          var mondayMidnight = moment().startOf('week');
+          var saturdayMidnight = moment().startOf('week').add(6, 'days');
+          return eventStartTime >= mondayMidnight && eventStartTime <= saturdayMidnight;
+        case 'This Weekend':
+          var fridayMidnight = moment().startOf('week').add(5, 'days');
+          var nextMondayMidnight = moment().startOf('week').add(8, 'days');
+          return eventStartTime >= fridayMidnight && eventStartTime <= nextMondayMidnight;
+        case 'Next Week':
+          var nextMonday = moment().startOf('week').add(8, 'days');
+          var nextSundayMidnight = moment().startOf('week').add(14, 'days');
+          return eventStartTime >= nextMonday && eventStartTime <= nextSundayMidnight;
+        case 'This Month':
+          return eventStartTime >= moment().startOf('month') && eventStartTime <= moment().startOf('month').add(1, 'month');
+      }
+    };
+
+    $scope.selectEvent = function(e, route){
+      if(route === 'register') {
+        $location.path('/register/' + e.id + '/page/');
+      }else{
+        $location.path('/' + route + '/' + e.id);
+      }
+    };
+
+    $scope.userAdminForEvent = function(eventId){
+      return _.contains(loggedInUserEvents, eventId);
     };
   });
