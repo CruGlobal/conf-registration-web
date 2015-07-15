ed71c6bd932af7df69ef1afa7ab80b1213cc6323
adammeyer <adam.meyer@cru.org>
2015-07-15 12:50:55 -0400

    jshint

--- test/spec/controllers/landing.js
+++ test/spec/controllers/landing.js
@@ -18,9 +18,9 @@ describe('Controller: landingCtrl', function () {
 
   it('dateFilter should return true', function () {
     expect(scope.dateFilter({
-      "id": "34e4f769-3b80-44e1-88b5-xxxxxx",
-      "eventStartTime": moment(),
-      "eventEndTime": moment().add(4, 'days')
+      'id': '34e4f769-3b80-44e1-88b5-xxxxxx',
+      'eventStartTime': moment(),
+      'eventEndTime': moment().add(4, 'days')
     })).toBe(true);
   });
 });
