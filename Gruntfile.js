ce6d570dd3de56a30bcd82033fc72e69dd6dbe1f
adammeyer <adam.meyer@cru.org>
2015-12-01 10:22:38 -0500

    rollbar error notify

--- Gruntfile.js
+++ Gruntfile.js
@@ -101,6 +101,7 @@ module.exports = function (grunt) {
       all: [
         'Gruntfile.js',
         '<%= yeoman.app %>/scripts/{,*/}*.js',
+        '!<%= yeoman.app %>/scripts/errorNotify.js',
         'test/spec/{,*/}*.js'
       ]
     },
