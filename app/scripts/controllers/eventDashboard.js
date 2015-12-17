9aa754b797db0e084a79d48d572714af5e4f8999
adammeyer <adam.meyer@cru.org>
2015-12-17 17:03:58 -0500

    Recent events list

--- app/scripts/controllers/eventDashboard.js
+++ app/scripts/controllers/eventDashboard.js
@@ -11,18 +11,9 @@ angular.module('confRegistrationWebApp')
       footer: true
     };
 
-    $scope.conferences = conferences;
-    $scope.filterName = '';
-    $scope.resetFilterName = function(){
-      $scope.filterName = '';
-    };
-
-    $scope.$watch('showArchivedEvents', function (v) {
-      if (v) {
-        $scope.showArchivedEventsFilter = '';
-      } else {
-        $scope.showArchivedEventsFilter = false;
-      }
+    $scope.conferences = _.map(conferences, function(c){
+      c.lastAccess = localStorage.getItem('lastAccess:' + c.id);
+      return c;
     });
 
     $scope.createEvent = function () {
