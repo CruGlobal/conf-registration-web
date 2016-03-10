b6f0f58bd02ce10de013da4cdf32f52d70be5de9
adammeyer <adam.meyer@cru.org>
2016-03-10 16:15:51 -0500

    expense type in payments modal

--- app/scripts/directives/page.js
+++ app/scripts/directives/page.js
@@ -3,10 +3,10 @@
 angular.module('confRegistrationWebApp')
   .directive('page', function () {
     return {
-      templateUrl: function() {
-        var eventQuestionEditor = window.location.hash.indexOf('eventForm') !== -1;
-        return eventQuestionEditor ? 'views/components/pageEditor.html' : 'views/components/pageRegistration.html';
-      },
-      restrict: 'E'
+      templateUrl: 'views/components/pageDirective.html',
+      restrict: 'E',
+      controller: function ($scope, $location) {
+        $scope.wizard = $location.path().indexOf('eventForm') !== -1;
+      }
     };
   });
