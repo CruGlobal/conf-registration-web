6514d5594c795bd0a4bcdf2b45f622e3d3b5b801
Jayakrishnan V <jayakrishnan.v@anlonits.com>
2016-09-22 17:20:35 +0530

    Design fix for range information Design fix for min and max input in advance tab

--- app/scripts/directives/limitToRange.js
+++ app/scripts/directives/limitToRange.js
@@ -10,10 +10,10 @@ angular.module('confRegistrationWebApp')
             scope.$watch('value', function (newVal, oldVal) {
                 if ((angular.isUndefined(newVal) && !angular.isUndefined(oldVal) && !angular.isUndefined(attributes.max))
                     || (!angular.isUndefined(newVal) && attributes.max && Number(newVal) > Number(attributes.max))) {
-                    scope.value = null;
+                    scope.value = "";
                 } else if ((angular.isUndefined(newVal) && !angular.isUndefined(oldVal) && !angular.isUndefined(attributes.min))
                     || (!angular.isUndefined(newVal) && attributes.min != "" && Number(newVal) < Number(attributes.min))) {
-                    scope.value = null;
+                    scope.value = "";
                 }
             }, true);
         }
