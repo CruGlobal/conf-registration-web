e23bf26a25d8e2f0f8ff47217d106746c6216ae4
Lee Braddock <lee.braddock@cru.org>
2016-10-06 11:13:08 -0400

    Rules support for any match in a [subset] of dropdown or multiple choice answers or checkbox as per https://jira.cru.org/browse/EVENT-432

--- app/scripts/services/validateRegistrant.js
+++ app/scripts/services/validateRegistrant.js
@@ -4,26 +4,36 @@ angular.module('confRegistrationWebApp')
   .service('validateRegistrant', function validateRegistrant() {
 
     var blockVisibleRuleCheck = function(block, registrant){
-      var returnValue = true;
       var answers = registrant.answers;
-      angular.forEach(block.rules, function(rule){
-        var answer = _.find(answers, {blockId: rule.parentBlockId});
-        if(angular.isUndefined(answer) || answer.value === ''){
-          returnValue = false;
-        }else{
-          if(rule.operator === '=' && answer.value !== rule.value) {
-            returnValue = false;
-          }else if(rule.operator === '!=' && answer.value === rule.value){
-            returnValue = false;
-          }else if(rule.operator === '>' && answer.value <= rule.value){
-            returnValue = false;
-          }else if(rule.operator === '<' && answer.value >= rule.value){
-            returnValue = false;
+      var ruleOperand = block.content && block.content.ruleoperand ? block.content.ruleoperand : 'AND';
+      var validRuleCount = 0;
+
+      for (var i = 0; i < block.rules.length; i++) {
+        var rule = block.rules[i];
+        var answer = _.find(answers, { blockId: rule.parentBlockId });
+        if (angular.isDefined(answer) && answer.value !== '') {
+          if (rule.operator === '=' && answer.value === rule.value) {
+            validRuleCount++;
+          } else if (rule.operator === '!=' && answer.value !== rule.value) {
+            validRuleCount++;
+          } else if (rule.operator === '>' && answer.value > rule.value) {
+            validRuleCount++;
+          } else if (rule.operator === '<' && answer.value < rule.value) {
+            validRuleCount++;
           }
         }
-      });
+        if ((ruleOperand === 'OR' && validRuleCount > 0) || (ruleOperand === 'AND' && validRuleCount <= i)) {
+          break;
+        }
+      }
 
-      return returnValue;
+      if (block.rules.length === 0 || // If no rules are set
+        (ruleOperand === 'OR' && validRuleCount > 0) ||
+        (ruleOperand === 'AND' && validRuleCount === block.rules.length)) {
+        return true;
+      } else {
+        return false;
+      }
     };
 
     var blockInRegistrantType = function(block, registrant){
