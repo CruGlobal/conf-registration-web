27d02e35700b9c0b5fbfe706e1273757b7ec1ce5
Scotty Waggoner <ozzieorca@gmail.com>
2016-10-06 14:26:15 -0700

    Add show-errors-instant to skip $touched check in show-errors
    - Add show-errors-instant to number min/max inputs and number rule inputs

--- app/scripts/directives/showErrors.js
+++ app/scripts/directives/showErrors.js
@@ -15,13 +15,13 @@ angular.module('confRegistrationWebApp')
         }
 
         scope.$watch(function(scope){
-            return ngModelCtrl.$invalid && (scope.currentPageVisited || ngModelCtrl.$touched);
+            return ngModelCtrl.$invalid && (scope.currentPageVisited || ngModelCtrl.$touched || !!attrs.showErrorsInstant);
           },
           function(invalid){
             if(scope.inputs && scope.inputs.length >= 2){
               //if we are handling a group of inputs and any of the inputs are invalid and touched
               var groupInvalid = scope.inputs.some(function(currentValue){
-                  return currentValue.$invalid && (scope.currentPageVisited || currentValue.$touched);
+                  return currentValue.$invalid && (scope.currentPageVisited || currentValue.$touched || !!attrs.showErrorsInstant);
                 });
               element.toggleClass('has-no-error', !invalid);
               element.parents('.form-group').toggleClass('has-error', groupInvalid);
