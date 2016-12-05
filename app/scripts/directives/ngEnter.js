f318c7540c25543915dbd9f055096637f9ef81e5
Caleb Cox <canac@users.noreply.github.com>
2016-12-05 10:31:45 -0500

    Upgrade to jQuery 3

--- app/scripts/directives/ngEnter.js
+++ app/scripts/directives/ngEnter.js
@@ -2,7 +2,7 @@
 
 angular.module('confRegistrationWebApp').directive('ngEnter', function () {
   return function (scope, element, attrs) {
-    element.bind('keypress', function (event) {
+    element.on('keypress', function (event) {
       if (event.which === 13) {
         scope.$apply(function () {
           scope.$eval(attrs.ngEnter);
