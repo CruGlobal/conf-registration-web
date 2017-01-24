fb9abd6fed9a35cc675171cce3e76793822076c4
Caleb Cox <canac@users.noreply.github.com>
2017-01-24 14:17:20 -0500

    Tokenize credit cards via TSYS

--- app/scripts/services/payment.js
+++ app/scripts/services/payment.js
@@ -1,16 +1,46 @@
 'use strict';
 
 angular.module('confRegistrationWebApp')
-  .factory('payment', function ($q, $http, $filter, error) {
+  .factory('payment', function ($q, $http, $filter, cruPayments, error) {
+    // Load the TSYS manifest
+    // Returns a promise that resolves to the manifest value
+    function loadTsysManifest (conference, payment) {
+      var url = 'payments/manifest/' + conference.id;
+      return $http.get(url, { data: payment }).then(function (res) {
+        return res.data;
+      });
+    }
+
     // Modify a credit card payment to use a tokenized credit card instead of real credit card data
-    function tokenizeCreditCardPayment (payment) {
+    // Tokenize the credit card via Authorize.NET
+    function tokenizeCreditCardPaymentAuthorizeNet (conference, payment) {
       return $http.get('payments/ccp-client-encryption-key').then(function (res) {
         var ccpClientEncryptionKey = res.data;
         ccp.initialize(ccpClientEncryptionKey);
         payment.creditCard.lastFourDigits = ccp.getAbbreviatedNumber(payment.creditCard.number);
         payment.creditCard.number = ccp.encrypt(payment.creditCard.number);
         payment.creditCard.cvvNumber = ccp.encrypt(payment.creditCard.cvvNumber);
-      }).catch(error.errorFromResponse('An error occurred while requesting the ccp encryption key. Please try your payment again.'));
+      }).catch(error.errorFromResponse('An error occurred while requesting the Authorize.NET token. Please try your payment again.'));
+    }
+
+    // Modify a credit card payment to use a tokenized credit card instead of real credit card data
+    // Tokenize the credit card via TSYS
+    function tokenizeCreditCardPaymentTsys (conference, payment) {
+      return $q.when()
+        .then(function () {
+          return loadTsysManifest(conference, payment);
+        })
+        .then(function (manifest) {
+          cruPayments.init('staging', conference.paymentGatewayId, manifest);
+          return cruPayments.encrypt(payment.creditCard.number, payment.creditCard.cvvNumber,
+                                     payment.creditCard.expirationMonth, payment.creditCard.expirationYear).toPromise();
+        })
+        .then(function (tokenizedCard) {
+          payment.creditCard.lastFourDigits = tokenizedCard.maskedCardNumber;
+          payment.creditCard.number = tokenizedCard.tsepToken;
+          payment.creditCard.cvvNumber = tokenizedCard.cvv2;
+        })
+        .catch(error.errorFromResponse('An error occurred while requesting the TSYS token. Please try your payment again.'));
     }
 
     return {
@@ -35,7 +65,7 @@ angular.module('confRegistrationWebApp')
       },
 
       // Submit payment for a current registration
-      pay: function (payment, registration, acceptedPaymentMethods) {
+      pay: function (payment, conference, registration, acceptedPaymentMethods) {
         if (Number(payment.amount) === 0 || !acceptedPaymentMethods || payment.paymentType === 'PAY_ON_SITE') {
           // No payment is necessary, so no work needs to be done here
           return $q.when();
@@ -49,7 +79,13 @@ angular.module('confRegistrationWebApp')
         return $q.when().then(function () {
           if (currentPayment.paymentType === 'CREDIT_CARD') {
             // Credit card payments must be tokenized first
-            return tokenizeCreditCardPayment(currentPayment);
+            if (conference.paymentGatewayType === 'TSYS') {
+              return tokenizeCreditCardPaymentTsys(conference, currentPayment);
+            } else if (conference.paymentGatewayType === 'AUTHORIZE_NET') {
+              return tokenizeCreditCardPaymentAuthorizeNet(conference, currentPayment);
+            } else {
+              throw new Error('Unrecognized payment gateway.');
+            }
           }
         }).then(function () {
           // Submit the payment
