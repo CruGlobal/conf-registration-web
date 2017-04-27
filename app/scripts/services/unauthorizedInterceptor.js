fc08d80e19e3f1ff15823db1d0a687df8e611c6d
Scotty Waggoner <ozzieorca@gmail.com>
2017-04-27 00:32:07 -0700

    Update $cookies usages to match newer Angular API and fix logout route which was leaving a trailing slash after a string replace

--- app/scripts/services/unauthorizedInterceptor.js
+++ app/scripts/services/unauthorizedInterceptor.js
@@ -4,7 +4,7 @@ angular.module('confRegistrationWebApp')
   .factory('unauthorizedInterceptor', function ($q, $cookies, loginDialog) {
     return {
       responseError: function (rejection) {
-        if (rejection.status === 401 && angular.isDefined($cookies.crsToken)) {
+        if (rejection.status === 401 && angular.isDefined($cookies.get('crsToken'))) {
           loginDialog.show(true);
         }
         return $q.reject(rejection);
