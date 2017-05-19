bee315c8842d66ed51df9029c563735faffbe94f
adammeyer <adam.meyer@cru.org>
2017-05-19 16:40:53 -0400

    dynamically load dtm

--- app/scripts/factories/analytics.js
+++ app/scripts/factories/analytics.js
@@ -1,6 +1,15 @@
 'use strict';
 
-angular.module('confRegistrationWebApp').factory('analytics', function ($window, $rootScope, ProfileCache) {
+angular.module('confRegistrationWebApp').factory('analytics', function ($window, $rootScope, ProfileCache, ADOBE_ANALYTICS_STAGING, ADOBE_ANALYTICS_PROD) {
+  $window.digitalData = {};
+
+  //load analytics JS
+  jQuery.ajax({
+    url: $window.isStaging ? ADOBE_ANALYTICS_STAGING : ADOBE_ANALYTICS_PROD,
+    dataType: 'script',
+    cache: true
+  });
+
   return {
     available: angular.isDefined($window._satellite),
     digitalData: $window.digitalData,
@@ -26,4 +35,5 @@ angular.module('confRegistrationWebApp').factory('analytics', function ($window,
     },
     track: angular.isDefined($window._satellite) && $window._satellite.track
   };
-});
+}).constant('ADOBE_ANALYTICS_STAGING', '//assets.adobedtm.com/3202ba9b02b459ee20779cfcd8e79eaf266be170/satelliteLib-928beaae1bce68f8836cc63dd18a9951aa3a1f4e-staging.js')
+  .constant('ADOBE_ANALYTICS_PROD', '//assets.adobedtm.com/3202ba9b02b459ee20779cfcd8e79eaf266be170/satelliteLib-928beaae1bce68f8836cc63dd18a9951aa3a1f4e.js');
