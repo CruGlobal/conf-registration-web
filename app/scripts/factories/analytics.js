21b9055f1a14fb33832986104700aaecaa976cdb
adammeyer <adam.meyer@cru.org>
2017-05-22 11:58:09 -0400

    include analytics via processhtml includes

--- app/scripts/factories/analytics.js
+++ app/scripts/factories/analytics.js
@@ -1,14 +1,7 @@
 'use strict';
 
-angular.module('confRegistrationWebApp').factory('analytics', function ($window, $rootScope, ProfileCache, jQuery, ADOBE_ANALYTICS_STAGING, ADOBE_ANALYTICS_PROD) {
+angular.module('confRegistrationWebApp').factory('analytics', function ($window, $rootScope, ProfileCache) {
   $window.digitalData = {};
-
-  //load analytics JS
-  jQuery.ajax({
-    url: $window.isStaging ? ADOBE_ANALYTICS_STAGING : ADOBE_ANALYTICS_PROD,
-    dataType: 'script',
-    cache: true
-  });
 
   return {
     available: angular.isDefined($window._satellite),
@@ -35,5 +28,4 @@ angular.module('confRegistrationWebApp').factory('analytics', function ($window,
     },
     track: angular.isDefined($window._satellite) && $window._satellite.track
   };
-}).constant('ADOBE_ANALYTICS_STAGING', '//assets.adobedtm.com/3202ba9b02b459ee20779cfcd8e79eaf266be170/satelliteLib-928beaae1bce68f8836cc63dd18a9951aa3a1f4e-staging.js')
-  .constant('ADOBE_ANALYTICS_PROD', '//assets.adobedtm.com/3202ba9b02b459ee20779cfcd8e79eaf266be170/satelliteLib-928beaae1bce68f8836cc63dd18a9951aa3a1f4e.js');
+});
