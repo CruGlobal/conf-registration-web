c2d2e6f9c83d9544df1fce12caa41e6ec98e4a6e
Scotty Waggoner <ozzieorca@gmail.com>
2017-08-11 19:48:47 -0700

    Cleanup dateFilter
    - Fix failing test

--- test/spec/controllers/landing.spec.js
+++ test/spec/controllers/landing.spec.js
@@ -8,20 +8,49 @@ describe('Controller: landingCtrl', function () {
 
   beforeEach(angular.mock.inject(function($rootScope, $controller) {
     scope = $rootScope.$new();
-    scope.eventFilters = {
-      date: 'This Week'
-    };
 
     $controller('landingCtrl', {
       $scope: scope
     });
   }));
 
-  it('dateFilter should return true', function () {
-    expect(scope.dateFilter({
-      'id': '34e4f769-3b80-44e1-88b5-xxxxxx',
-      'eventStartTime': moment(),
-      'eventEndTime': moment().add(4, 'days')
-    })).toBe(true);
+  describe('dateFilter', () => {
+    beforeEach(() => {
+      jasmine.clock().mockDate(moment('2017-03-01').toDate());
+      scope.eventFilters = {
+        date: null
+      };
+    });
+
+    it('should return true if not set', function () {
+      expect(scope.dateFilter({ 'eventStartTime': moment() })).toEqual(true);
+      expect(scope.dateFilter({ 'eventStartTime': moment().add(1, 'year') })).toEqual(true);
+    });
+    it('should return true for This Week', function () {
+      scope.eventFilters.date = 'This Week';
+      expect(scope.dateFilter({ 'eventStartTime': moment().add(3, 'days') })).toEqual(true);
+      expect(scope.dateFilter({ 'eventStartTime': moment().add(1, 'weeks') })).toEqual(false);
+    });
+    it('should return true for Next Week', function () {
+      scope.eventFilters.date = 'Next Week';
+      expect(scope.dateFilter({ 'eventStartTime': moment().add(3, 'days') })).toEqual(false);
+      expect(scope.dateFilter({ 'eventStartTime': moment().add(1, 'weeks') })).toEqual(true);
+    });
+    it('should return true for This Month', function () {
+      scope.eventFilters.date = 'This Month';
+      expect(scope.dateFilter({ 'eventStartTime': moment().add(1, 'weeks') })).toEqual(true);
+      expect(scope.dateFilter({ 'eventStartTime': moment().add(3, 'weeks') })).toEqual(true);
+      expect(scope.dateFilter({ 'eventStartTime': moment().add(2, 'months') })).toEqual(false);
+    });
+    it('should return true for Next Month', function () {
+      scope.eventFilters.date = 'Next Month';
+      expect(scope.dateFilter({ 'eventStartTime': moment().add(1, 'weeks') })).toEqual(false);
+      expect(scope.dateFilter({ 'eventStartTime': moment().add(1, 'months') })).toEqual(true);
+    });
+    it('should return true for Greater Than Next Month', function () {
+      scope.eventFilters.date = 'Greater Than Next Month';
+      expect(scope.dateFilter({ 'eventStartTime': moment().add(1, 'months') })).toEqual(false);
+      expect(scope.dateFilter({ 'eventStartTime': moment().add(2, 'months') })).toEqual(true);
+    });
   });
 });
