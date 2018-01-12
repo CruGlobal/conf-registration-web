b103d0e9f344622e89641b52984cf1ebbb1d2c65
Bill Randall <william.randall@cru.org>
2018-01-12 15:13:45 -0500

    Fix lint issue

--- test/spec/controllers/landing.spec.js
+++ test/spec/controllers/landing.spec.js
@@ -26,27 +26,32 @@ describe('Controller: landingCtrl', function () {
       expect(scope.dateFilter({ 'eventStartTime': moment() })).toEqual(true);
       expect(scope.dateFilter({ 'eventStartTime': moment().add(1, 'year') })).toEqual(true);
     });
+
     it('should return true for This Week', function () {
       scope.eventFilters.date = 'This Week';
       expect(scope.dateFilter({ 'eventStartTime': moment().add(3, 'days') })).toEqual(true);
       expect(scope.dateFilter({ 'eventStartTime': moment().add(1, 'weeks') })).toEqual(false);
     });
+
     it('should return true for Next Week', function () {
       scope.eventFilters.date = 'Next Week';
       expect(scope.dateFilter({ 'eventStartTime': moment().add(3, 'days') })).toEqual(false);
       expect(scope.dateFilter({ 'eventStartTime': moment().add(1, 'weeks') })).toEqual(true);
     });
+
     it('should return true for This Month', function () {
       scope.eventFilters.date = 'This Month';
       expect(scope.dateFilter({ 'eventStartTime': moment().add(1, 'weeks') })).toEqual(true);
       expect(scope.dateFilter({ 'eventStartTime': moment().add(3, 'weeks') })).toEqual(true);
       expect(scope.dateFilter({ 'eventStartTime': moment().add(2, 'months') })).toEqual(false);
     });
+
     it('should return true for Next Month', function () {
       scope.eventFilters.date = 'Next Month';
       expect(scope.dateFilter({ 'eventStartTime': moment().add(1, 'weeks') })).toEqual(false);
       expect(scope.dateFilter({ 'eventStartTime': moment().add(1, 'months') })).toEqual(true);
     });
+
     it('should return true for Greater Than Next Month', function () {
       scope.eventFilters.date = 'Greater Than Next Month';
       expect(scope.dateFilter({ 'eventStartTime': moment().add(1, 'months') })).toEqual(false);
