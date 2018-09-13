89623db7009a3286fbc84399e972bb211eeec5ae
Emiliano Qaqi <emilianoqaqi@msn.com>
2018-09-13 11:52:28 -0300

    EVENT-555 add tests

---
+++ test/spec/services/DateRangeService.spec.js
@@ -0,0 +1,23 @@
+import 'angular-mocks';
+
+describe('Service: DateRangeService', function () {
+
+  beforeEach(angular.mock.module('confRegistrationWebApp'));
+
+  var DateRangeService;
+  beforeEach(inject(function (_DateRangeService_) {
+    DateRangeService = _DateRangeService_;
+  }));
+
+  it('should call the callback when subscribed', function () {
+    var object = {};
+    object.callback = function () {};
+
+    const callbackSpy = spyOn(object, 'callback').and.callThrough();
+
+    DateRangeService.subscribe('sample-uuid', object.callback);
+    DateRangeService.emitChange('sample-uuid', 'sample-date');
+
+    expect(callbackSpy).toHaveBeenCalled();
+  });
+});
