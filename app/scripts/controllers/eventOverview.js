902d1d6b82f418c84955f2efd22860a4e0cdb917
katefree <>
2020-02-20 00:24:04 +0200

    EVENT-661: Ability to easily add a logo or picture for the event. Tests added, code review fixes

--- app/scripts/controllers/eventOverview.js
+++ app/scripts/controllers/eventOverview.js
@@ -2,77 +2,79 @@
 
 angular
   .module('confRegistrationWebApp')
-  .controller('eventOverviewCtrl', function(
-    $rootScope,
-    $scope,
-    $location,
-    $route,
-    $http,
-    $sce,
-    ConfCache,
-    conference,
-  ) {
-    $rootScope.globalPage = {
-      type: 'admin',
-      mainClass: 'container event-overview',
-      bodyClass: '',
-      confId: conference.id,
-      footer: true,
-    };
+  .controller(
+    'eventOverviewCtrl',
+    (
+      $rootScope,
+      $scope,
+      $location,
+      $route,
+      $http,
+      $sce,
+      ConfCache,
+      conference,
+    ) => {
+      $rootScope.globalPage = {
+        type: 'admin',
+        mainClass: 'container event-overview',
+        bodyClass: '',
+        confId: conference.id,
+        footer: true,
+      };
 
-    $scope.resetImage = () => {
-      $scope.imageSrc = conference.image;
-      $scope.includeImageToAllPages = conference.includeImageToAllPages;
-    };
+      $scope.resetImage = () => {
+        $scope.imageSrc = conference.image;
+        $scope.includeImageToAllPages = conference.includeImageToAllPages;
+      };
 
-    $scope.conference = conference;
-    $scope.imageSrc = conference.image;
-    $scope.selectedImage = '';
-    $scope.resetImage();
+      $scope.conference = conference;
+      $scope.selectedImage = '';
+      $scope.resetImage();
 
-    $scope.deleteImage = () => {
-      $scope.imageSrc = '';
-      $scope.includeImageToAllPages = false;
-      $scope.saveImage();
-    };
+      $scope.deleteImage = () => {
+        $scope.imageSrc = '';
+        $scope.includeImageToAllPages = false;
+        $scope.saveImage();
+      };
 
-    $scope.saveImage = () => {
-      $http({
-        method: 'PUT',
-        url: 'conferences/' + conference.id + '/image',
-        data: {
-          image: $scope.imageSrc,
-          includeImageToAllPages: $scope.includeImageToAllPages,
-        },
-      }).then(function() {
-        $scope.conference.image = $scope.imageSrc;
-        $scope.conference.includeImageToAllPages =
-          $scope.includeImageToAllPages;
-        ConfCache.update(conference.id, $scope.conference);
-        $scope.notify = {
-          class: 'alert-success',
-          message: $sce.trustAsHtml(
-            '<strong>Saved!</strong> Event image details have been updated.',
-          ),
-        };
+      $scope.saveImage = () => {
+        $http({
+          method: 'PUT',
+          url: `conferences/${conference.id}/image`,
+          data: {
+            image: $scope.imageSrc,
+            includeImageToAllPages: $scope.includeImageToAllPages,
+          },
+        }).then(() => {
+          $scope.conference.image = $scope.imageSrc;
+          $scope.conference.includeImageToAllPages =
+            $scope.includeImageToAllPages;
+          ConfCache.update(conference.id, $scope.conference);
+          $scope.notify = {
+            class: 'alert-success',
+            message: $sce.trustAsHtml(
+              '<strong>Saved!</strong> Event image details have been updated.',
+            ),
+          };
+        });
+      };
+
+      var port = '';
+      if ($location.$$port !== 80 && $location.$$port !== 443) {
+        port = ':' + $location.$$port;
+      }
+      var baseUrl =
+        $location.$$protocol +
+        '://' +
+        $location.$$host +
+        port +
+        '/register/' +
+        conference.id;
+      $scope.registrationUrl = {};
+      $scope.registrationUrl[0] = baseUrl;
+
+      angular.forEach(conference.registrantTypes, t => {
+        $scope.registrationUrl[t.id] = baseUrl + '?regType=' + t.id;
       });
-    };
-
-    var port = '';
-    if ($location.$$port !== 80 && $location.$$port !== 443) {
-      port = ':' + $location.$$port;
-    }
-    var baseUrl =
-      $location.$$protocol +
-      '://' +
-      $location.$$host +
-      port +
-      '/register/' +
-      conference.id;
-    $scope.registrationUrl = {};
-    $scope.registrationUrl[0] = baseUrl;
-
-    angular.forEach(conference.registrantTypes, function(t) {
-      $scope.registrationUrl[t.id] = baseUrl + '?regType=' + t.id;
-    });
-  });
+    },
+  );
