789aea8421d7343641c009488105397a200b63c7
katefree <katefree@gmail.com>
2020-04-06 08:56:59 +0300

    EVENT-678: Pre-fill International chartfields

--- test/spec/directives/payment.spec.js
+++ test/spec/directives/payment.spec.js
@@ -57,4 +57,68 @@ describe('Directive: ertPayment', function() {
 
     expect(scope.currentPayment.transfer.accountNumber).toBe('');
   });
+
+  it('accountTypeChanged to NON_US_STAFF should pre-fill businessUnit and department', () => {
+    scope.currentPayment = {
+      transfer: {
+        accountType: 'NON_US_STAFF',
+        department: 'department',
+        businessUnit: 'businessUnit',
+      },
+    };
+    scope.accountTypeChanged();
+
+    expect(scope.currentPayment.transfer.businessUnit).toBe('INTLM');
+    expect(scope.currentPayment.transfer.department).toBe('GENL');
+  });
+
+  it('accountTypeChanged to something not equal to NON_US_STAFF should not pre-fill businessUnit and department', () => {
+    scope.currentPayment = {
+      transfer: {
+        accountType: 'not-NON_US_STAFF',
+        department: 'department',
+        businessUnit: 'businessUnit',
+      },
+    };
+    scope.accountTypeChanged();
+
+    expect(scope.currentPayment.transfer.businessUnit).toBe('');
+    expect(scope.currentPayment.transfer.department).toBe('');
+  });
+
+  it('validatePayment should validate required TRANSFER NON_US_STAFF operatingUnit field', () => {
+    scope.currentPayment = {
+      paymentType: 'TRANSFER',
+      transfer: {
+        accountType: 'NON_US_STAFF',
+        department: 'GENL',
+        businessUnit: 'INTLM',
+        operatingUnit: '',
+        accountNumber: 'accountNumber',
+      },
+    };
+    scope.validatePayment(scope.currentPayment);
+
+    expect(scope.currentPayment.errors).toContain(
+      'Please fill in Operating Unit and Account Number fields.',
+    );
+  });
+
+  it('validatePayment should validate required TRANSFER NON_US_STAFF accountNumber field', () => {
+    scope.currentPayment = {
+      paymentType: 'TRANSFER',
+      transfer: {
+        accountType: 'NON_US_STAFF',
+        department: 'GENL',
+        businessUnit: 'INTLM',
+        operatingUnit: 'operatingUnit',
+        accountNumber: '',
+      },
+    };
+    scope.validatePayment(scope.currentPayment);
+
+    expect(scope.currentPayment.errors).toContain(
+      'Please fill in Operating Unit and Account Number fields.',
+    );
+  });
 });
