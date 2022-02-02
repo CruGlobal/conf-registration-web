8ff7fda54c4cf0f3e610e0f599130e4f2edb406a
TheNoodleMoose <cjh605@gmail.com>
2022-02-02 09:49:14 -0500

    Add more tests for event address formatting

--- test/spec/filters/eventAddressFormat.spec.js
+++ test/spec/filters/eventAddressFormat.spec.js
@@ -11,9 +11,58 @@ describe('Filter: eventAddressFormat', () => {
     });
   });
 
-  it('should format address', () => {
+  it('should format address | City, State Zip', () => {
     expect(filter('City', 'FL', '55555', 'US')).toEqual({
       addressLine3: 'City, FL 55555',
+      addressCountry: 'United States',
+    });
+  });
+
+  it('should format address | City, State', () => {
+    expect(filter('City', 'FL', null, 'US')).toEqual({
+      addressLine3: 'City, FL',
+      addressCountry: 'United States',
+    });
+  });
+
+  it('should format address | City, Zip', () => {
+    expect(filter('City', null, '55555', 'US')).toEqual({
+      addressLine3: 'City 55555',
+      addressCountry: 'United States',
+    });
+  });
+
+  it('should format address | Just City', () => {
+    expect(filter('City', null, null, 'US')).toEqual({
+      addressLine3: 'City',
+      addressCountry: 'United States',
+    });
+  });
+
+  it('should format address | State Zip', () => {
+    expect(filter(null, 'FL', '55555', 'US')).toEqual({
+      addressLine3: 'FL 55555',
+      addressCountry: 'United States',
+    });
+  });
+
+  it('should format address | Just State', () => {
+    expect(filter(null, 'FL', null, 'US')).toEqual({
+      addressLine3: 'FL',
+      addressCountry: 'United States',
+    });
+  });
+
+  it('should format address | Just Zip', () => {
+    expect(filter(null, null, '55555', 'US')).toEqual({
+      addressLine3: '55555',
+      addressCountry: 'United States',
+    });
+  });
+
+  it('should format address | No address', () => {
+    expect(filter(null, null, null, 'US')).toEqual({
+      addressLine3: '',
       addressCountry: 'United States',
     });
   });
