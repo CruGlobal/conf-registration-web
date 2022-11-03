ea120b2fe50c3e3809463fe8246530030ada6e1d
Jacob Pinchook <jacob.pinchook@gmail.com>
2022-11-03 14:13:52 -0400

    groupModal creation converting showGroupModal.html with react2angular

---
+++ pages/groupModal.tsx
@@ -0,0 +1,163 @@
+// React creation
+
+// import { useState } from 'react';
+// import angular from 'angular';
+// import { react2angular } from 'react2angular';
+// import 'styles/style.scss';
+
+// interface GroupModalProps {
+//   resolve: {
+//     conference: any;
+//     queryParameters: any;
+//   };
+//   envService: any;
+//   $cookies: {
+//     get(key: string): string;
+//   };
+//   modalInstance: {
+//     dismiss: () => void;
+//   };
+// }
+
+// enum AccountTransferExportEnum {
+//   REGISTRATION = 'REGISTRATION',
+//   MISCELLANEOUS_ITEM = 'MISCELLANEOUS_ITEM',
+//   CHILDCARE = 'CHILDCARE',
+//   STAFF_TAXABLE_ITEM = 'STAFF_TAXABLE_ITEM',
+// }
+
+// const GroupModal = ({
+//   resolve,
+//   modalInstance,
+//   $cookies,
+//   envService,
+// }: GroupModalProps) => {
+//   const { conference, queryParameters } = resolve;
+
+//   const handleClose = () => modalInstance.dismiss();
+// });
+
+//   return (
+//     <>
+//       <div className="modal-header">
+//         <button type="button" className="close" onClick={handleClose}>
+//             &times;
+//         </button>
+//         <h4 translate="yes">{conference.name} Group</h4>
+//       </div>
+//       <div className="modal-body">
+//         <table className="table review-breakdown">
+//           <thead>
+//             <tr>
+//               <th translate="yes">Registrant</th>
+//               <th width="110"><span translate="yes">Type</span></th>
+//               <th width="150"></th>
+//             </tr>
+//           </thead>
+//           <tbody
+//           ng-repeat="r in $ctrl.getRegistration($ctrl.registrationId).groupRegistrants | orderBy: 'createdTimestamp'"
+//           >
+//             <tr>
+//               <td>
+//                 {{r.firstName}} {{r.lastName}}
+//                 <span
+//                   ng-if="r.id === $ctrl.getRegistration($ctrl.registrationId).primaryRegistrantId"
+//                   translate="yes"
+//                   >(Group Creator)</span
+//                 >
+//               </td>
+//               <td>
+//                 <span>{{$ctrl.getRegistrantType(r.registrantTypeId).name}}</span>
+//               </td>
+//               <td className="text-right">
+//                 <input
+//                   type="button"
+//                   className="btn btn-sm btn-default btn-bold"
+//                   ng-click="$ctrl.editRegistrant(r)"
+//                   value="Edit"
+//                 />
+//                 <input
+//                   type="button"
+//                   className="btn btn-sm btn-danger btn-bold"
+//                   ng-click="$ctrl.deleteRegistrant(r)"
+//                   ng-if="r.id !== $ctrl.getRegistration($ctrl.registrationId).primaryRegistrantId"
+//                   value="Remove"
+//                 />
+//               </td>
+//             </tr>
+//           </tbody>
+//         </table>
+
+//         <div
+//           className="row reg-type-select"
+//           ng-repeat="type in $ctrl.visibleRegistrantTypes"
+//           ng-if="$ctrl.visibleRegistrantTypes.length > 1"
+//         >
+//           <div
+//             className="stacked-spacing-col-sm spacing-xs"
+//             ng-className="{'col-sm-7': type.cost>0, 'col-sm-9': type.cost==0}"
+//           >
+//             <h4>{{type.name}}</h4>
+//             <span className="display-linebreaks">{{type.description}}</span>
+//           </div>
+//           <div
+//             className="text-right"
+//             ng-class="{'col-sm-5': type.cost>0, 'col-sm-3': type.cost==0}"
+//           >
+//             <span className="cost" ng-if="type.cost"
+//               >{{type.calculatedCurrentCost |
+//               localizedCurrency:conference.currency.currencyCode}}</span
+//             >
+//             <a
+//               href=""
+//               ng-click="$ctrl.register(type.id)"
+//               className="btn btn-sm btn-success btn-important"
+//               ng-if="!$ctrl.registrationTypeFull(type)"
+//               translate="yes"
+//             >
+//               Register
+//             </a>
+//             <a
+//               href=""
+//               className="btn btn-sm btn-success btn-important btn-full"
+//               disabled
+//               ng-if="$ctrl.registrationTypeFull(type)"
+//               translate="yes"
+//             >
+//               Full
+//             </a>
+//           </div>
+//         </div>
+//         <div className="row" ng-if="$ctrl.visibleRegistrantTypes.length === 1">
+//           <div className="col-xs-12 text-center">
+//             <a
+//               href
+//               ng-click="$ctrl.register($ctrl.visibleRegistrantTypes[0].id)"
+//               className="btn btn-success btn-important btn-lg"
+//               ng-if="!$ctrl.registrationTypeFull($ctrl.visibleRegistrantTypes[0])"
+//               translate="yes"
+//             >
+//               Register
+//             </a>
+//             <p
+//               ng-if="$ctrl.registrationTypeFull($ctrl.visibleRegistrantTypes[0])"
+//               translate="yes"
+//             >
+//               Sorry, this event is full.
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="modal-footer">
+//         <button ng-click="$ctrl.dismiss()" className="btn btn-default" translate="yes">
+//           Close
+//         </button>
+//       </div>
+//     </>
+//   );
+
+// export default groupModal;
+
+// angular
+//   .module('react.components', [])
+//   .component('groupModal', react2angular(groupModal, []));
