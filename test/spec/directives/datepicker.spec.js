a5bdcce9ca4fceed2c22e45745b9062036d27506
Caleb Alldrin <caleb.alldrin@cru.org>
2023-09-13 15:39:22 -0700

    Reorganize datepicker.js and add tests

--- test/spec/directives/datepicker.spec.js
+++ test/spec/directives/datepicker.spec.js
@@ -18,19 +18,33 @@ describe('Directive: datepicker', function () {
     scope = element.isolateScope() || element.scope();
   }));
 
-  it('Sets the date to the correct format based on the type of date question', function () {
+  it('Sets the date to the correct format for the graduation date question datepicker', function () {
+    scope.monthYearOnly = true;
     scope.updateTimeStamp(new Date('02/05/1994'));
 
     expect(scope.localModel).toBe('1994-02-10');
 
+    scope.updateTimeStamp(new Date('02/05/1994 10:11:12'));
+
+    //removes time and sets the day to 10
+    expect(scope.localModel).toBe('1994-02-10');
+
+    expect(scope.dateOptions.viewMode).toBe('years');
+  });
+
+  it('Sets the date to the correct format for all other types of datepickers', function () {
     scope.monthYearOnly = false;
 
     scope.updateTimeStamp(new Date('02/05/1994'));
 
+    //adds the time and reformats
     expect(scope.localModel).toBe('1994-02-05 00:00:00');
   });
 
-  it('Sets date options correctly based on type of date', function () {
-    expect(scope.dateOptions.viewMode).toBe('years');
+  it('Sets the initial/default date to the correct value and format based on the localModel', function () {
+    let localModel = new Date('01-30-2023 11:01:05');
+    let dateOptions = scope.getDateOptions(localModel);
+
+    expect(dateOptions.defaultDate).toBe('2023-01-30 11:01:05');
   });
 });
