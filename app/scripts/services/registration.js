44688671edadcdd43c448c8bc4620246448bc800
Caleb Cox <canac@users.noreply.github.com>
2025-09-04 15:44:40 -0500

    Make internal methods private

--- app/scripts/services/registration.js
+++ app/scripts/services/registration.js
@@ -17,57 +17,55 @@ angular
         });
       }
 
-      return {
-        // Validate payment information for a registration
-        validatePayment: function (
-          currentPayment,
-          currentRegistration,
-          conference,
-        ) {
-          if (
-            payment.validate(currentPayment, currentRegistration, conference)
-          ) {
-            return $q.when();
-          } else {
-            modalMessage.error({
-              title: 'Please correct the following errors:',
-              message: currentPayment.errors,
-            });
-            return $q.reject();
+      // Validate payment information for a registration
+      function validatePayment(
+        currentPayment,
+        currentRegistration,
+        conference,
+      ) {
+        if (payment.validate(currentPayment, currentRegistration, conference)) {
+          return $q.when();
+        } else {
+          modalMessage.error({
+            title: 'Please correct the following errors:',
+            message: currentPayment.errors,
+          });
+          return $q.reject();
+        }
+      }
+
+      // Mark a current registration as completed
+      function completeRegistration(currentRegistration) {
+        return $q(function (resolve, reject) {
+          var registration = angular.copy(currentRegistration);
+
+          if (registration.completed) {
+            // The registration is already completed, so nothing needs to be done
+            resolve();
+            return;
           }
-        },
 
-        // Mark a current registration as completed
-        completeRegistration: function (currentRegistration) {
-          return $q(function (resolve, reject) {
-            var registration = angular.copy(currentRegistration);
-
-            if (registration.completed) {
-              // The registration is already completed, so nothing needs to be done
+          registration.completed = true;
+          RegistrationCache.update(
+            'registrations/' + registration.id,
+            registration,
+            function () {
+              RegistrationCache.emptyCache();
               resolve();
-              return;
-            }
-
-            registration.completed = true;
-            RegistrationCache.update(
-              'registrations/' + registration.id,
-              registration,
-              function () {
-                RegistrationCache.emptyCache();
-                resolve();
-              },
-              function (response) {
-                currentRegistration.completed = false;
-                reject(response);
-              },
-            );
-          }).catch(
-            error.errorFromResponse(
-              'An error occurred while completing your registration.',
-            ),
+            },
+            function (response) {
+              currentRegistration.completed = false;
+              reject(response);
+            },
           );
-        },
+        }).catch(
+          error.errorFromResponse(
+            'An error occurred while completing your registration.',
+          ),
+        );
+      }
 
+      return {
         // Process complete registration flow for one or more registrations
         processRegistrations: function (
           registrations,
@@ -76,39 +74,33 @@ angular
         ) {
           return $q
             .when()
-            .then(
-              function () {
-                const validationPromises = registrations.map((item) =>
-                  this.validatePayment(
-                    currentPayment,
-                    item.registration,
-                    item.conference,
-                  ),
-                );
-                return $q.all(validationPromises);
-              }.bind(this),
-            )
-            .then(
-              function () {
-                const paymentPromises = registrations.map((item) =>
-                  payment.pay(
-                    currentPayment,
-                    item.conference,
-                    item.registration,
-                    acceptedPaymentMethods,
-                  ),
-                );
-                return $q.all(paymentPromises);
-              }.bind(this),
-            )
-            .then(
-              function () {
-                const completionPromises = registrations.map((item) =>
-                  this.completeRegistration(item.registration),
-                );
-                return $q.all(completionPromises);
-              }.bind(this),
-            )
+            .then(function () {
+              const validationPromises = registrations.map((item) =>
+                validatePayment(
+                  currentPayment,
+                  item.registration,
+                  item.conference,
+                ),
+              );
+              return $q.all(validationPromises);
+            })
+            .then(function () {
+              const paymentPromises = registrations.map((item) =>
+                payment.pay(
+                  currentPayment,
+                  item.conference,
+                  item.registration,
+                  acceptedPaymentMethods,
+                ),
+              );
+              return $q.all(paymentPromises);
+            })
+            .then(function () {
+              const completionPromises = registrations.map((item) =>
+                completeRegistration(item.registration),
+              );
+              return $q.all(completionPromises);
+            })
             .then(
               function () {
                 registrations.forEach(({ conference }) => {
