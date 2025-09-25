0a76b4ec960f7dcfc95495dca56499fe32623b6f
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2025-09-25 09:00:05 -0500

    Change admin group modal to only show dependents that are allowed children of the primary registrant type

--- app/scripts/directives/showGroupModal.js
+++ app/scripts/directives/showGroupModal.js
@@ -23,37 +23,95 @@ angular.module('confRegistrationWebApp').component('showGroupModal', {
 
       const registration = this.getRegistration(this.registrationId);
 
-      _.remove(this.visibleRegistrantTypes, function (registrantType) {
-        //remove if type is marked as hidden and a registrant with this type doesn't already exist in the registration
-        return (
-          registrantType.hidden &&
-          !_.includes(
-            _.map(registration.registrants, 'registrantTypeId'),
-            registrantType.id,
-          )
-        );
+      // Group parent/primary
+      const primaryRegistrant = _.find(registration.groupRegistrants, {
+        id: registration.primaryRegistrantId,
       });
+
+      // Get primary registrant type
+      const primaryRegistrantType = this.getRegistrantType(
+        primaryRegistrant ? primaryRegistrant.registrantTypeId : null,
+      );
+
+      // Get dependent registrant types
+      const primaryRegistrantTypeSet =
+        primaryRegistrantType && primaryRegistrantType.allowedRegistrantTypeSet
+          ? primaryRegistrantType.allowedRegistrantTypeSet
+          : [];
+
+      // Filter visibleRegistrantTypes to only include those
+      // that are children of the primary registrant type
+      const childIds = _.map(primaryRegistrantTypeSet, 'childRegistrantTypeId');
+      this.visibleRegistrantTypes = _.filter(
+        this.visibleRegistrantTypes,
+        function (type) {
+          return _.includes(childIds, type.id);
+        },
+      );
     };
 
     this.register = function (typeId) {
-      this.registerUser(this.getRegistration(this.registrationId), typeId);
+      this.registerUser(
+        this.getRegistration(this.registrationId),
+        typeId,
+        true,
+      );
     };
 
+    /*
+     * The old implementation of this function did not work correctly,
+     * as useLimit and availableSlots are not managed correctly.
+     * This implementation takes advantage of allowedRegistrantTypeSet's
+     * numberOfChildRegistrants to determine the dependent limit to reach
+     * the intended behavior.
+     * */
     this.registrationTypeFull = function (type) {
-      if (!type.useLimit) {
+      const registration = this.getRegistration(this.registrationId);
+      if (!registration || !registration.groupRegistrants) {
         return false;
       }
-      if (!type.availableSlots) {
-        return true;
+
+      const primaryRegistrant = _.find(registration.groupRegistrants, {
+        id: registration.primaryRegistrantId,
+      });
+      if (!primaryRegistrant) {
+        return false;
       }
 
-      const registration = this.getRegistration(this.registrationId);
-
-      // returns true when this registration type is full
-      return (
-        type.availableSlots <=
-        _.filter(registration.registrants, { registrantTypeId: type.id }).length
+      const primaryRegistrantType = this.getRegistrantType(
+        primaryRegistrant.registrantTypeId,
       );
+      if (
+        !primaryRegistrantType ||
+        !primaryRegistrantType.allowedRegistrantTypeSet
+      ) {
+        return false;
+      }
+
+      const dependent = _.find(
+        primaryRegistrantType.allowedRegistrantTypeSet,
+        (set) => set.childRegistrantTypeId === type.id,
+      );
+      if (!dependent) {
+        return false;
+      }
+
+      const maxAllowedDependents = dependent.numberOfChildRegistrants;
+      // numberOfChildRegistrants === 0 means no limit
+      if (!maxAllowedDependents) {
+        return false;
+      }
+
+      // Count current dependents of this type in the group
+      if (!registration.groupRegistrants) {
+        return true;
+      }
+      const currentCount = _.filter(registration.groupRegistrants, {
+        registrantTypeId: type.id,
+      }).length;
+
+      // Return true if the group has reached the allowed limit for this dependent type
+      return currentCount >= maxAllowedDependents;
     };
   },
 });
