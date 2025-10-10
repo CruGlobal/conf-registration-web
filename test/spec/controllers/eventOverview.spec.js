6d4094649bf7bd558044987b124b60876da15228
Caleb Cox <canac@users.noreply.github.com>
2025-10-10 15:20:58 -0500

    Remove other test

--- test/spec/controllers/eventOverview.spec.js
+++ test/spec/controllers/eventOverview.spec.js
@@ -5,46 +5,23 @@ describe('Controller: eventOverview', () => {
 
   let scope;
   let testData;
-  let RegistrationCache;
-  let $q;
   let $rootScope;
 
   beforeEach(
-    angular.mock.inject(
-      (_$rootScope_, $controller, _testData_, _RegistrationCache_, _$q_) => {
-        testData = _testData_;
-        RegistrationCache = _RegistrationCache_;
-        $q = _$q_;
-        $rootScope = _$rootScope_;
-        scope = $rootScope.$new();
+    angular.mock.inject((_$rootScope_, $controller, _testData_) => {
+      testData = _testData_;
+      $rootScope = _$rootScope_;
+      scope = $rootScope.$new();
 
-        spyOn(RegistrationCache, 'getAllForConference').and.returnValue(
-          $q.resolve({
-            registrations: [],
-          }),
-        );
-
-        $controller('eventOverviewCtrl', {
-          $scope: scope,
-          $rootScope: $rootScope,
-          conference: testData.conference,
-        });
-      },
-    ),
+      $controller('eventOverviewCtrl', {
+        $scope: scope,
+        $rootScope: $rootScope,
+        conference: testData.conference,
+      });
+    }),
   );
 
   it('sets registrationUrl', () => {
     expect(scope.registrationUrl).toBeDefined();
-  });
-
-  it('should call RegistrationCache.getAllForConference on initialization', () => {
-    expect(RegistrationCache.getAllForConference).toHaveBeenCalledWith(
-      testData.conference.id,
-      {
-        includeCheckedin: 'yes',
-        includeWithdrawn: 'no',
-        includeIncomplete: 'no',
-      },
-    );
   });
 });
