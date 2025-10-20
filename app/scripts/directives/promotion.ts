b0d80c4c4b147f527f6325f06008d7aeb5dd0ee4
Caleb Cox <canac@users.noreply.github.com>
2025-10-20 15:09:18 -0500

    Make promotion conference optional

--- app/scripts/directives/promotion.ts
+++ app/scripts/directives/promotion.ts
@@ -7,10 +7,11 @@ import template from 'views/components/promotion.html';
 interface PromotionScope extends IScope {
   // Bindings
   promo: Promotion;
-  conference: Conference;
+  conference?: Conference;
   onDelete: () => void;
 
   // Local state and methods
+  currencyCode: string;
   expanded: boolean;
   toggleExpanded: () => void;
   toggleRegistrantType: (id: string) => void;
@@ -26,8 +27,9 @@ angular.module('confRegistrationWebApp').directive('promotion', function () {
       onDelete: '&',
     },
     controller: function ($scope: PromotionScope) {
-      $scope.expanded = false;
+      $scope.currencyCode = $scope.conference?.currency.currencyCode ?? 'USD';
 
+      $scope.expanded = false;
       $scope.toggleExpanded = function () {
         $scope.expanded = !$scope.expanded;
       };
