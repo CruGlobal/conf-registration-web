e7a32c6a12796e83776498c791cd3b2fb2532722
Caleb Cox <canac@users.noreply.github.com>
2025-10-20 14:02:23 -0500

    Add tests

---
+++ test/spec/directives/promotion.spec.js
@@ -0,0 +1,47 @@
+import 'angular-mocks';
+
+describe('Directive: promotion', function () {
+  beforeEach(angular.mock.module('confRegistrationWebApp'));
+
+  let element, scope;
+  beforeEach(inject(function ($compile, $rootScope, $templateCache, testData) {
+    $templateCache.put('views/components/promotion.html', '');
+
+    scope = $rootScope.$new();
+    scope.promo = testData.conference.promotions[0];
+    scope.conference = testData.conference;
+    scope.onDelete = jasmine.createSpy('onDelete');
+
+    element = $compile(
+      '<promotion promo="promo" conference="conference" on-delete="onDelete()" />',
+    )(scope);
+    scope.$digest();
+    scope = element.isolateScope() || element.scope();
+  }));
+
+  it('toggleExpanded should toggle expanded', function () {
+    expect(scope.expanded).toBe(false);
+
+    scope.toggleExpanded();
+
+    expect(scope.expanded).toBe(true);
+
+    scope.toggleExpanded();
+
+    expect(scope.expanded).toBe(false);
+  });
+
+  it('toggleRegistrantType should toggle registrant type id', function () {
+    const removedId = scope.promo.registrantTypeIds[0];
+    scope.toggleRegistrantType(removedId);
+
+    expect(scope.promo.registrantTypeIds).not.toContain(removedId);
+    expect(scope.promo.registrantTypeIds.length).toBe(1);
+
+    const addedId = scope.conference.registrantTypes[2].id;
+    scope.toggleRegistrantType(addedId);
+
+    expect(scope.promo.registrantTypeIds).toContain(addedId);
+    expect(scope.promo.registrantTypeIds.length).toBe(2);
+  });
+});
