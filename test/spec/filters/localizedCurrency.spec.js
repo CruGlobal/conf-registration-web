4396af86c6fe3298578a70eb99ea6e812c976e51
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2025-10-24 14:41:13 -0500

    Try to fix localizedCurrency reading undefined

--- test/spec/filters/localizedCurrency.spec.js
+++ test/spec/filters/localizedCurrency.spec.js
@@ -16,6 +16,11 @@ describe('Filter: localizedCurrency', function () {
 
     expect(filter(amount, 'USD')).toContain('123.12');
   });
+
+  it('should handle null and undefined values by formatting as zero', function () {
+    expect(filter(null, 'USD')).toContain('0');
+    expect(filter(undefined, 'USD')).toContain('0');
+  });
 });
 
 describe('Filter: localizedSymbol', function () {
