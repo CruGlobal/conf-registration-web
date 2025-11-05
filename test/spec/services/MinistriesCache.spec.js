f3c771054571a1903a361ae4e7818785985864a0
Caleb Cox <canac@users.noreply.github.com>
2025-11-05 10:51:29 -0600

    Update MinistriesCache to testData ministries

--- test/spec/services/MinistriesCache.spec.js
+++ test/spec/services/MinistriesCache.spec.js
@@ -1,96 +1,21 @@
 import 'angular-mocks';
 
-const mockMinistries = [
-  {
-    id: 'acfbef45-57a4-4a9b-a527-a3d57e98b66b',
-    name: 'Military',
-    activities: [],
-    strategies: [],
-    eventTypes: [],
-  },
-  {
-    id: 'f6d8e4c6-60c1-4c59-9495-d6c3eb65cac1',
-    name: 'Lifelines',
-    strategies: [],
-    activities: [
-      {
-        id: 'e7aab792-4bce-4032-85b4-35f73d8db46b',
-        name: 'Rock Climbing',
-      },
-      {
-        id: '8a577943-2acb-4c6f-8a86-a3c442332610',
-        name: 'General',
-      },
-      {
-        id: 'd4970a28-e63d-4e51-b8d7-c337d5662cb3',
-        name: 'Canoeing/Kayaking',
-      },
-    ],
-    eventTypes: [],
-  },
-  {
-    id: 'f6d31fe3-7078-4fac-a37b-9596d57558e9',
-    name: 'Campus - National Team/Strategy',
-    strategies: [
-      {
-        id: '15d3a7ae-93c9-40ff-97f4-9f31f7036f21',
-        name: 'Destino',
-      },
-      {
-        id: '9769eb02-1075-45fc-ae03-c7733627a1ef',
-        name: 'N/A',
-      },
-      {
-        id: 'd7d42d11-59b0-4d1f-9906-3ab706c63e8a',
-        name: 'Bridges',
-      },
-      {
-        id: '7eeae1c0-205a-4435-9557-a721a81604d3',
-        name: 'Epic',
-      },
-    ],
-    activities: [],
-    eventTypes: [
-      {
-        id: 'cfc2b308-566b-432b-bee4-4ed60fec5608',
-        name: 'Fall Retreat/Getaway',
-      },
-      {
-        id: '272907e5-7c2b-4ba5-97b8-1ed7e07c290b',
-        name: 'N/A',
-      },
-      {
-        id: '0f87dff6-0115-4d86-8bc7-5e785334b3e2',
-        name: 'Spring Break',
-      },
-    ],
-  },
-  {
-    id: '87b02878-5070-473b-bb07-3b2d899b46d6',
-    name: 'Athletes in Action',
-    strategies: [],
-    activities: [
-      {
-        id: '22532e40-f458-4ae6-b045-24815e104013',
-        name: 'SCRC Events',
-      },
-      {
-        id: '31073156-9599-4a51-9b61-2bb6dbbf447b',
-        name: 'Domestic Projects',
-      },
-    ],
-    eventTypes: [],
-  },
-];
-
 describe('Service: MinistriesCache', () => {
   beforeEach(angular.mock.module('confRegistrationWebApp'));
 
-  let MinistriesCache, $httpBackend, $rootScope;
-  beforeEach(inject((_MinistriesCache_, _$httpBackend_, _$rootScope_) => {
+  let MinistriesCache, $httpBackend, $rootScope, ministries;
+  beforeEach(inject((
+    _MinistriesCache_,
+    _$httpBackend_,
+    _$rootScope_,
+    testData,
+  ) => {
     $httpBackend = _$httpBackend_;
     $rootScope = _$rootScope_;
     MinistriesCache = _MinistriesCache_;
+    ministries = testData.ministries;
+
+    $httpBackend.expectGET('ministries').respond(200, ministries);
   }));
 
   afterEach(() => {
@@ -100,46 +25,41 @@ describe('Service: MinistriesCache', () => {
 
   describe('get', () => {
     it('should fetch and sort data', () => {
-      $httpBackend.expectGET('ministries').respond(200, mockMinistries);
-
-      let ministries;
+      let loadedMinistries;
       MinistriesCache.get().then((result) => {
-        ministries = result;
+        loadedMinistries = result;
       }, fail);
 
       $httpBackend.flush();
       $rootScope.$apply();
 
-      expect(ministries.map(({ name }) => name)).toEqual([
+      expect(loadedMinistries.map(({ name }) => name)).toEqual([
         'Athletes in Action',
         'Campus - National Team/Strategy',
+        'Family Life',
         'Lifelines',
-        'Military',
       ]);
 
-      expect(ministries[2].activities.map(({ name }) => name)).toEqual([
+      expect(loadedMinistries[3].activities.map(({ name }) => name)).toEqual([
         'Canoeing/Kayaking',
         'General',
+        'Paintball',
         'Rock Climbing',
+        'White Water Rafting',
       ]);
 
-      expect(ministries[1].strategies.map(({ name }) => name)).toEqual([
+      expect(loadedMinistries[1].strategies.map(({ name }) => name)).toEqual([
         'Bridges',
-        'Destino',
-        'Epic',
         'N/A',
       ]);
 
-      expect(ministries[1].eventTypes.map(({ name }) => name)).toEqual([
+      expect(loadedMinistries[1].eventTypes.map(({ name }) => name)).toEqual([
         'Fall Retreat/Getaway',
-        'Spring Break',
-        'N/A',
+        'Mobilization/Recruiting',
       ]);
     });
 
     it('should use cached data on subsequent calls', () => {
-      $httpBackend.expectGET('ministries').respond(200, mockMinistries);
-
       MinistriesCache.get().catch(fail);
       $httpBackend.flush();
       MinistriesCache.get().catch(fail);
@@ -148,6 +68,7 @@ describe('Service: MinistriesCache', () => {
     });
 
     it('should handle HTTP errors', () => {
+      $httpBackend.resetExpectations();
       $httpBackend.expectGET('ministries').respond(500, 'Server Error');
 
       let errorCaught = false;
@@ -163,7 +84,6 @@ describe('Service: MinistriesCache', () => {
 
   describe('with loaded ministries', () => {
     beforeEach(() => {
-      $httpBackend.expectGET('ministries').respond(200, mockMinistries);
       MinistriesCache.get().catch(fail);
       $httpBackend.flush();
       $rootScope.$apply();
@@ -171,8 +91,8 @@ describe('Service: MinistriesCache', () => {
 
     describe('getMinistryName', () => {
       it('should return ministry name', () => {
-        expect(MinistriesCache.getMinistryName(mockMinistries[1].id)).toBe(
-          mockMinistries[1].name,
+        expect(MinistriesCache.getMinistryName(ministries[0].id)).toBe(
+          ministries[0].name,
         );
       });
 
@@ -183,10 +103,10 @@ describe('Service: MinistriesCache', () => {
 
     describe('getActivityName', () => {
       it('should return activity name', () => {
-        const activity = mockMinistries[1].activities[0];
+        const activity = ministries[0].activities[0];
 
         expect(
-          MinistriesCache.getActivityName(mockMinistries[1].id, activity.id),
+          MinistriesCache.getActivityName(ministries[0].id, activity.id),
         ).toBe(activity.name);
       });
 
@@ -201,7 +121,7 @@ describe('Service: MinistriesCache', () => {
 
       it('should return null for non-existent activity', () => {
         const name = MinistriesCache.getActivityName(
-          mockMinistries[1].id,
+          ministries[1].id,
           'non-existent-activity-id',
         );
 
