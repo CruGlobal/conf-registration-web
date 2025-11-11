0715f3f39df95742dbac4bcb9f9f4142aca0a9d7
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2025-11-11 09:12:39 -0600

    Change faIcon to component directive

--- app/scripts/directives/faIcon.js
+++ app/scripts/directives/faIcon.js
@@ -1,19 +1,39 @@
 /**
  * @ngdoc directive
  * @name confRegistrationWebApp.directive:faIcon
- * @restrict A
+ * @restrict E
  * @description
  * Adds aria-hidden="true" to Font Awesome icons for accessibility.
  * Decorative icons should be hidden from screen readers.
  *
- * @example
- * <i class="fa fa-check" fa-icon></i>
+ * @example <fa-icon icon="users"></fa-icon>
  */
 angular.module('confRegistrationWebApp').directive('faIcon', function () {
   return {
-    restrict: 'A',
-    link: function (_scope, element) {
-      element.attr('aria-hidden', 'true');
+    restrict: 'E',
+    scope: {
+      icon: '@?',
+    },
+    link: function (scope, element, attrs) {
+      const iconElement = angular.element('<i></i>');
+
+      iconElement.addClass('fa');
+
+      if (scope.icon) {
+        iconElement.addClass('fa-' + scope.icon);
+      }
+
+      if (attrs.class) {
+        iconElement.addClass(attrs.class);
+      }
+
+      if (attrs.ngClass) {
+        iconElement.attr('ng-class', attrs.ngClass);
+      }
+
+      iconElement.attr('aria-hidden', 'true');
+
+      element.replaceWith(iconElement);
     },
   };
 });
