63242703e7962ebc8a2d7b246221ec0e37c639c4
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2026-01-21 10:21:01 -0600

    Minor refactor to payment type formatting and add unit tests for the filter

--- app/scripts/filters/paymentTypeFormat.js
+++ app/scripts/filters/paymentTypeFormat.js
@@ -5,12 +5,11 @@ angular
       if (angular.isUndefined(paymentType)) {
         return '';
       }
-      if (paymentType === 'FL_GIFT_CARD') {
-        return 'Gift Card';
-      }
-      return (
+
+      let paymentTypeString =
         paymentType.charAt(0).toUpperCase() +
-        paymentType.substring(1).toLowerCase()
-      );
+        paymentType.substring(1).toLowerCase();
+      paymentTypeString = paymentTypeString.replaceAll('_', ' ');
+      return paymentTypeString;
     };
   });
