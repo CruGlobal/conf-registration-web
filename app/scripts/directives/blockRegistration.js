4971d20ece972434d0b44794185b3b01f8bc4624
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2026-02-06 16:49:38 -0600

    Revert "Add visibility check for registration blocks and clear answers if hidden"
    This reverts commit 702bc5adc99e3253659ddacdffd811a43425d8c6.

--- app/scripts/directives/blockRegistration.js
+++ app/scripts/directives/blockRegistration.js
@@ -205,19 +205,15 @@ angular
           ) {
             return false;
           }
-          const registrant = _.find($scope.currentRegistration.registrants, {
+          var registrant = _.find($scope.currentRegistration.registrants, {
             id: $scope.currentRegistrant,
           });
-          const isVisible = validateRegistrant.blockVisible(
+          return validateRegistrant.blockVisible(
             block,
             registrant,
             false,
             $scope.conference,
           );
-
-          clearAnswerIfBlockHidden(isVisible, block);
-
-          return isVisible;
         };
 
         function clearAnswerIfOptionHidden(isVisible, block, $scope, choice) {
@@ -232,33 +228,6 @@ angular
           ) {
             $scope.answer.value = null;
           }
-        }
-
-        function clearAnswerIfBlockHidden(isVisible, block) {
-          if (isVisible) {
-            return;
-          }
-
-          const blockTypesToClear = [
-            'emailQuestion',
-            'textQuestion',
-            'textareaQuestion',
-            'phoneQuestion',
-            'dateQuestion',
-            'nameQuestion',
-            'addressQuestion',
-          ];
-          if (!blockTypesToClear.includes(block.type)) {
-            return;
-          }
-
-          if ($scope.answer.value) {
-            return;
-          }
-          $scope.answer.value = getDefaultValue(
-            block.type,
-            block.content && block.content.default,
-          );
         }
 
         function setForceSelections(block, registrant, isVisible, choice) {
