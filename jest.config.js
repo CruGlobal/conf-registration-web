2202d1ca38887cc534d6de4bba6ec761dc2c258b
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2026-05-15 15:20:59 -0500

    Review changes

--- jest.config.js
+++ jest.config.js
@@ -15,8 +15,7 @@ const config = {
     '\\.(html)$': '<rootDir>/__tests__/templateMock.ts',
   },
   roots: ['<rootDir>/app/scripts/'],
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/*.test.[jt]s?(x)'],
-  testPathIgnorePatterns: ['/node_modules/', '\\.spec\\.js$'],
+  testMatch: ['**/*.test.[jt]s?(x)'],
   setupFilesAfterEnv: ['<rootDir>/__tests__/util/setup.ts'],
   testEnvironment: 'jsdom',
 };
