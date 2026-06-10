0dbfe91457b98310c31b544fa5f3ebbc64644ef9
Caleb Cox <canac@users.noreply.github.com>
2026-06-10 14:04:06 -0500

    Simplify currency symbol extraction

--- app/scripts/filters/localizedCurrency.js
+++ app/scripts/filters/localizedCurrency.js
@@ -5,7 +5,7 @@ angular
       if (number === null || number === undefined) {
         return '';
       }
-      let localeId = $locale.id ? $locale.id : 'en-us';
+      const localeId = $locale.id ? $locale.id : 'en-us';
       return number.toLocaleString(localeId, {
         style: 'currency',
         currency: currencyCode || 'USD',
@@ -15,45 +15,16 @@ angular
 
 angular
   .module('confRegistrationWebApp')
-  .filter('localizedSymbol', ($locale, $window) => {
+  .filter('localizedSymbol', ($locale) => {
     return (currencyCode) => {
-      let localeId = $locale && $locale.id ? $locale.id : 'en-us';
-      let symbol = symbolFromFormatToParts(
-        localeId,
-        currencyCode || 'USD',
-        $window,
-      );
-      if (symbol) {
-        return symbol;
-      }
-      let number = 0;
-      return number
-        .toLocaleString(localeId, {
-          style: 'currency',
-          currency: currencyCode || 'USD',
-        })
-        .replace(/[0., ]/g, '');
+      const localeId = $locale && $locale.id ? $locale.id : 'en-us';
+      const numberFormat = new Intl.NumberFormat(localeId, {
+        style: 'currency',
+        currency: currencyCode || 'USD',
+      });
+      const currencySymbol = numberFormat
+        .formatToParts(0)
+        .find((part) => part.type === 'currency')?.value;
+      return currencySymbol;
     };
   });
-
-function symbolFromFormatToParts(localeId, currencyCode, window) {
-  if (!('Intl' in window)) {
-    return;
-  }
-  let numberFormat = new Intl.NumberFormat(localeId, {
-    style: 'currency',
-    currency: currencyCode,
-  });
-  if (!('formatToParts' in numberFormat)) {
-    return;
-  }
-  // Some browsers incorrectly return nan or Nan for the formatToParts function
-  // This will filter out all incorrect results.
-  const filteredSymbol = numberFormat
-    .formatToParts()
-    .filter((e) => e.type !== 'nan' || e.value !== 'NaN');
-
-  if (filteredSymbol.length > 0) {
-    return filteredSymbol.filter((e) => e.type === 'currency')[0].value;
-  }
-}
